50d8b67ac3c67ff9303529ee5312d7253aa79975
Zeev Manilovich <zeevmanilovich@gmail.com>
2024-02-22 17:51:55 +0200

    remove github actions

--- .github/scripts/rosetta/start_rosetta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) Mysten Labs, Inc.
-# SPDX-License-Identifier: BSD-3-Clause-Clear
-
-echo "Start Rosetta online server"
-sui-rosetta start-online-server --data-path ./data &
-
-echo "Start Rosetta offline server"
-sui-rosetta start-offline-server &
