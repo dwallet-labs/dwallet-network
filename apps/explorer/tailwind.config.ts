4a32708677b7a15f0c6353d59ed32ffb4bbe69e0
Shay malichi <shaymalichi@Shays-MacBook-Pro.local>
2024-04-17 13:34:49 +0300

    fix sizing

--- apps/explorer/tailwind.config.ts
+++ apps/explorer/tailwind.config.ts
@@ -4,7 +4,18 @@
 import preset from '@mysten/core/tailwind.config';
 import { type Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@mysten/ui/src/**/*.{js,jsx,ts,tsx}'],
 	presets: [preset],
+	theme: {
+		extend: {
+			width: {
+				'112': '28rem',
+				'120': '30rem',
+				'128': '32rem', // Adds w-128 with a value of 32rem
+			},
+		},
+	},
 } satisfies Partial<Config>;
+
+export default config;
