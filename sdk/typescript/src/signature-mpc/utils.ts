6b6a3153c68b03478c4db1c8ebd8bc8ac60c63c8
Omer Sadika <omersadika@gmail.com>
2024-04-18 02:01:46 +0300

    Change to @dwallet-network/dwallet.js

--- sdk/typescript/src/signature-mpc/utils.ts
+++ sdk/typescript/src/signature-mpc/utils.ts
@@ -1,27 +1,39 @@
-import {Keypair} from "../cryptography";
-import {SuiClient} from "../client";
-import {setTimeout} from "timers/promises";
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+import { setTimeout } from 'timers/promises';
 
-export async function fetchObjectBySessionId(sessionId: string, type: string,keypair: Keypair, client: SuiClient) {
-    let cursor = null;
-    while(true) {
-        const objects = await client.getOwnedObjects({ owner: keypair.toSuiAddress(), cursor: cursor });
-        // @ts-ignore
-        const objectsContent = await client.multiGetObjects({ ids: objects.data.map((o) => o.data?.objectId)!, options: { showContent: true } });
+import type { SuiClient } from '../client';
+import type { Keypair } from '../cryptography';
 
+export async function fetchObjectBySessionId(
+	sessionId: string,
+	type: string,
+	keypair: Keypair,
+	client: SuiClient,
+) {
+	let cursor = null;
+	while (true) {
+		const objects = await client.getOwnedObjects({ owner: keypair.toSuiAddress(), cursor: cursor });
+		const objectsContent = await client.multiGetObjects({
+			ids: objects.data.map((o) => o.data?.objectId!),
+			options: { showContent: true },
+		});
 
-        const objectsFiltered = objectsContent.map((o) => o.data?.content).filter((o) => {
-            // @ts-ignore
-            return o?.dataType == "moveObject" && o?.type == type && o.fields["session_id"] == sessionId;
-
-        });
-        if(objectsFiltered.length > 0) {
-            return objectsFiltered[0];
-        } else if (objects.hasNextPage) {
-            cursor = objects.nextCursor;
-        } else {
-            cursor = null;
-        }
-        await setTimeout(500);
-    }
+		const objectsFiltered = objectsContent
+			.map((o) => o.data?.content)
+			.filter((o) => {
+				return (
+					// @ts-ignore
+					o?.dataType == 'moveObject' && o?.type == type && o.fields['session_id'] == sessionId
+				);
+			});
+		if (objectsFiltered.length > 0) {
+			return objectsFiltered[0];
+		} else if (objects.hasNextPage) {
+			cursor = objects.nextCursor;
+		} else {
+			cursor = null;
+		}
+		await setTimeout(500);
+	}
 }
