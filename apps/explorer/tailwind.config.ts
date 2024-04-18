6add4bec85de88cf1a73311fd84217e661c813a1
shaymalichi <46198407+shaymalichi@users.noreply.github.com>
2024-04-18 13:22:33 +0300

    explorer: Fixed Testnet and Devnet menu route to dWallet rpc and change sizing of the textbox (#24)
    Co-authored-by: Shay malichi <shaymalichi@Shays-MacBook-Pro.local>

--- apps/explorer/tailwind.config.ts
+++ apps/explorer/tailwind.config.ts
@@ -4,7 +4,17 @@
 import preset from '@mysten/core/tailwind.config';
 import { type Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@mysten/ui/src/**/*.{js,jsx,ts,tsx}'],
 	presets: [preset],
+	theme: {
+		extend: {
+			width: {
+				'112': '28rem',
+				'128': '32rem',
+			},
+		},
+	},
 } satisfies Partial<Config>;
+
+export default config;
