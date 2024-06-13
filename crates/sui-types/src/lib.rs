954049ea25f147892bfd5040cba03e855b5418f5
Shay malichi <shaymalichi@Shays-MacBook-Pro-2.local>
2024-06-13 15:05:27 +0300

    wip

--- crates/sui-types/src/lib.rs
+++ crates/sui-types/src/lib.rs
@@ -78,8 +78,6 @@ pub mod versioned;
 pub mod zk_login_authenticator;
 pub mod zk_login_util;
 
-pub mod eth_dwallet_cap;
-
 #[cfg(any(test, feature = "test-utils"))]
 #[path = "./unit_tests/utils.rs"]
 pub mod utils;
