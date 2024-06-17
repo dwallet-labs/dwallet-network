fcb224abd6510f555c37ef7c389a6e964584d4ab
Shay malichi <shaymalichi@Shays-MacBook-Pro-2.local>
2024-06-17 16:56:52 +0300

    wip

--- crates/eth-light-client/src/lib.rs
+++ crates/eth-light-client/src/lib.rs
@@ -1,6 +1,6 @@
-mod utils;
-mod update;
-mod proof;
-mod config;
-mod eth_state;
-mod light_client;
+pub mod utils;
+pub mod update;
+pub mod proof;
+pub mod config;
+pub mod eth_state;
+pub mod light_client;
