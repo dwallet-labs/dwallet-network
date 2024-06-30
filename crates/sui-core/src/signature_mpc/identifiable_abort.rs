82d0a3c86f85e1bbf2ff7841af568743c5be7b52
Yael Abergel <yaelab200@gmail.com>
2024-06-30 15:26:37 +0300

    fix generate proof condition

--- crates/sui-core/src/signature_mpc/identifiable_abort.rs
+++ crates/sui-core/src/signature_mpc/identifiable_abort.rs
@@ -139,7 +139,7 @@ pub fn spawn_proof_generation_and_conditional_malicious_identification(
         let mut mut_state = sign_session_states.get_mut(&session_id).unwrap();
         let state = mut_state.clone();
 
-        if state.clone().proofs.unwrap().contains_key(&party_id) {
+        if !state.clone().proofs.unwrap().contains_key(&party_id) {
             let proofs = generate_proofs(&state, &failed_messages_indices);
             let proofs: Vec<_> = proofs.iter().map(|(proof, _)| proof.clone()).collect();
             mut_state.insert_proofs(party_id, proofs.clone());
