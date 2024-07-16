e0bdfc2a845a8ccf026ad9d6eae80d29bedb2781
Itay Levy <itaylevy134@gmail.com>
2024-07-16 18:58:59 +0300

    --wip-- [skip ci]

--- crates/signature-mpc/src/twopc_mpc_protocols/mod.rs
+++ crates/signature-mpc/src/twopc_mpc_protocols/mod.rs
@@ -364,6 +364,14 @@ pub fn decentralized_party_sign_verify_encrypted_signature_parts_prehash(
         .collect()
 }
 
+/**
+ * Identify the parties that acted maliciously during the decryption of the signature, after all the parties
+ * that were involved in decrypting it have sent a proof that they behaved honestly.
+ *
+ * In case one of the involved parties failed to generate a proof, only this party will be returned.
+ * If all the involved parties sent a proof, and some of the proofs are invalid, all the parties that
+ * their proof was invalid will be returned.
+ */
 pub fn identify_malicious_parties(
     verification_round_party: SignaturePartialDecryptionProofVerificationParty,
     partial_signature_decryption_shares: HashMap<PartyID, DecryptionShare>,
@@ -371,7 +379,7 @@ pub fn identify_malicious_parties(
     decryption_key_share_public_parameters: DecryptionPublicParameters,
     signature_partial_decryption_proofs: HashMap<PartyID, PartialDecryptionProof>,
     decrypters: Vec<PartyID>,
-) -> Vec<PartyID> // malicious_party_ids
+) -> Vec<PartyID>
 {
     let lagrange_coefficients: HashMap<PartyID, AdjustedLagrangeCoefficientSizedNumber> = decrypters
         .clone()
