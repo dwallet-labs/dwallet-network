2262d9672fa4553c821a0b74119f23a5fa5d6d1c
yarined <yarin2edri@gmail.com>
2024-07-29 17:44:49 +0300

    remove js map files

--- sdk/typescript/src/client/types/changes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=changes.js.map
