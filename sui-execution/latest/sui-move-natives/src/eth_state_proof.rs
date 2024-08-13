69b0889f0864857fcd02bb152542d9a2a1d2ccba
Yuval Elmoalem <yuval.elmo@dwalletlabs.com>
2024-08-13 17:53:01 +0300

    cherry-pick

--- sui-execution/latest/sui-move-natives/src/eth_state_proof.rs
+++ sui-execution/latest/sui-move-natives/src/eth_state_proof.rs
@@ -47,7 +47,48 @@ pub fn verify_message_proof(
     _ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    todo!();
+    // Load the cost parameters from the protocol config
+    let verify_message_proof_cost_params = &context
+        .extensions()
+        .get::<NativesCostTable>()
+        .eth_state_proof
+        .clone();
+
+    // Charge the base cost for this operation.
+    native_charge_gas_early_exit!(
+        context,
+        verify_message_proof_cost_params.verify_message_proof_cost_base
+    );
+
+    let cost = context.gas_used();
+    let proof = pop_arg!(args, Vector).to_vec_u8()?;
+
+    let Ok(proof) = bcs::from_bytes::<ProofResponse>(proof.as_slice()) else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT.into(),
+        ));
+    };
+
+    // Verify the account proof against the state root.
+    let is_valid_account_proof = verify_proof(
+        &proof.account_proof.proof.as_slice(),
+        &proof.account_proof.root,
+        &proof.account_proof.path,
+        &proof.account_proof.value,
+    );
+
+    if !is_valid_account_proof {
+        return Ok(NativeResult::ok(cost, smallvec![Value::bool(false)]));
+    }
+
+    let is_valid = verify_proof(
+        &proof.storage_proof.proof,
+        &proof.storage_proof.root,
+        &proof.storage_proof.path,
+        &proof.storage_proof.value,
+    );
+    Ok(NativeResult::ok(cost, smallvec![Value::bool(is_valid)]))
 }
 
 /***************************************************************************************************
@@ -60,7 +101,77 @@ pub(crate) fn verify_eth_state(
     _ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    todo!();
+    // Load the cost parameters from the protocol config
+    let verify_message_proof_cost_params = &context
+        .extensions()
+        .get::<NativesCostTable>()
+        .eth_state_proof
+        .clone();
+
+    // Charge the base cost for this operation.
+    native_charge_gas_early_exit!(
+        context,
+        verify_message_proof_cost_params.verify_eth_state_cost_base
+    );
+
+    let cost = context.gas_used();
+    let (current_eth_state, optimistic_update, finality_update, updates_vec) = (
+        pop_arg!(args, Vector).to_vec_u8()?,
+        pop_arg!(args, Vector).to_vec_u8()?,
+        pop_arg!(args, Vector).to_vec_u8()?,
+        pop_arg!(args, Vector).to_vec_u8()?,
+    );
+
+    let Ok(mut eth_state) =
+        serde_json::from_slice::<ConsensusStateManager<NimbusRpc>>(current_eth_state.as_slice())
+    else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT.into(),
+        ));
+    };
+
+    let Ok(updates_vec) = serde_json::from_slice::<Vec<Update>>(updates_vec.as_slice()) else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT.into(),
+        ));
+    };
+
+    let Ok(finality_update) = serde_json::from_slice::<FinalityUpdate>(finality_update.as_slice())
+    else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT.into(),
+        ));
+    };
+
+    let Ok(optimistic_update) =
+        serde_json::from_slice::<OptimisticUpdate>(optimistic_update.as_slice())
+    else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT.into(),
+        ));
+    };
+
+    let updates = AggregateUpdates {
+        updates: updates_vec,
+        finality_update,
+        optimistic_update,
+    };
+
+    let Ok((new_state_bcs, slot)) = process_eth_state_updates(&mut eth_state, updates) else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR.into(),
+        ));
+    };
+
+    Ok(NativeResult::ok(
+        cost,
+        smallvec![Value::vector_u8(new_state_bcs), Value::u64(slot)],
+    ))
 }
 
 /***************************************************************************************************
@@ -73,12 +184,58 @@ pub(crate) fn create_initial_eth_state_data(
     _ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
-    todo!();
+    // Load the cost parameters from the protocol config
+    let verify_message_proof_cost_params = &context
+        .extensions()
+        .get::<NativesCostTable>()
+        .eth_state_proof
+        .clone();
+
+    // Charge the base cost for this operation.
+    native_charge_gas_early_exit!(
+        context,
+        verify_message_proof_cost_params.create_initial_eth_state_data_cost_base
+    );
+
+    let cost = context.gas_used();
+
+    let (network, checkpoint) = (
+        pop_arg!(args, Vector).to_vec_u8()?,
+        pop_arg!(args, Vector).to_vec_u8()?,
+    );
+
+    let network = String::from_utf8(network)
+        .map_err(|_| PartialVMError::new(StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT))?;
+    let network = Network::from_str(&network)
+        .map_err(|_| PartialVMError::new(StatusCode::FAILED_TO_DESERIALIZE_ARGUMENT))?;
+
+    let eth_state = ConsensusStateManager::<NimbusRpc>::new_from_checkpoint(checkpoint, network);
+
+    let Ok(eth_state_bytes) = bcs::to_bytes(&eth_state) else {
+        return Ok(NativeResult::err(
+            cost,
+            StatusCode::VALUE_SERIALIZATION_ERROR.into(),
+        ));
+    };
+
+    Ok(NativeResult::ok(
+        cost,
+        smallvec![Value::vector_u8(eth_state_bytes)],
+    ))
 }
 
 fn process_eth_state_updates(
     eth_state: &mut ConsensusStateManager<NimbusRpc>,
     updates: AggregateUpdates,
 ) -> Result<(Vec<u8>, u64), PartialVMError> {
-    todo!();
+    eth_state.verify_and_apply_updates(&updates).map_err(|e| {
+        info!("Failed to verify updates: {:?}", e);
+        PartialVMError::new(StatusCode::UNKNOWN_INVARIANT_VIOLATION_ERROR)
+    })?;
+
+    let new_state_bytes = bcs::to_bytes(&eth_state)
+        .map_err(|_| PartialVMError::new(StatusCode::VALUE_SERIALIZATION_ERROR))?;
+    let slot = u64::from(eth_state.get_latest_slot());
+
+    Ok((new_state_bytes, slot))
 }
