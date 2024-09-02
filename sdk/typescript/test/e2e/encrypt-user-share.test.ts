89f474e7665283150e84e1592828ededcd71f5f4
Itay Levy <itaylevy134@gmail.com>
2024-09-02 11:56:11 +0300

    Fixed Scaly PR comments

--- sdk/typescript/test/e2e/encrypt-user-share.test.ts
+++ sdk/typescript/test/e2e/encrypt-user-share.test.ts
@@ -1,28 +1,20 @@
-import {
-	serialized_pubkeys_from_decentralized_dkg_output,
-	verify_user_share,
-} from '@dwallet-network/signature-mpc-wasm';
+import { serialized_pubkeys_from_decentralized_dkg_output } from '@dwallet-network/signature-mpc-wasm';
 import { beforeAll, describe, expect, it } from 'vitest';
 
 import {
 	createActiveEncryptionKeysTable,
 	createDWallet,
-	decrypt_user_share,
 	EncryptionKeyScheme,
 	generate_keypair,
-	generate_keypair_from_seed,
-	generate_proof,
 	getActiveEncryptionKeyObjID,
 	getEncryptedUserShareByObjectId,
-	getEncryptionKeyByObjectId,
 	setActiveEncryptionKey,
 	storeEncryptionKey,
-	transferEncryptedUserShare,
 } from '../../src/signature-mpc';
 import {
+	acceptUserShare,
 	getEncryptedUserShareByObjID,
 	sendUserShareToSuiPubKey,
-	verifyEncryptedSecretShare,
 } from '../../src/signature-mpc/encrypt_user_share';
 import { generatePaillierKeyPairFromSuiKeyPair } from '../../src/signature-mpc/utils';
 import { setup, TestToolbox } from './utils/setup';
@@ -34,62 +26,6 @@ describe('Secret key share transfer', () => {
 	beforeAll(async () => {
 		dwalletSenderToolbox = await setup();
 		dwalletReceiverToolbox = await setup();
-	});
-
-	it('should encrypt and transfer a dwallet to a newly generated public key', async () => {
-		const [encryptionKey, decryptionKey] = generate_keypair();
-		const pubKeyRef = await storeEncryptionKey(
-			encryptionKey,
-			EncryptionKeyScheme.Paillier,
-			dwalletSenderToolbox.keypair,
-			dwalletSenderToolbox.client,
-		);
-		const publicKeyID = pubKeyRef?.objectId;
-		const recipientData = await getEncryptionKeyByObjectId(
-			dwalletSenderToolbox.client,
-			publicKeyID,
-		);
-		const dwallet = await createDWallet(
-			dwalletSenderToolbox.keypair,
-			dwalletSenderToolbox.client,
-			encryptionKey,
-			publicKeyID,
-		);
-		const secretShare = dwallet?.secretKeyShare!;
-		const encryptedUserShareAndProof = generate_proof(
-			new Uint8Array(secretShare),
-			recipientData?.encryptionKey!,
-		);
-
-		// Verifies that the encryption key has been signed by the desired destination Sui address.
-		let isValidEncryptionKey = await dwalletSenderToolbox.keypair
-			.getPublicKey()
-			.verify(
-				new Uint8Array(recipientData?.encryptionKey!),
-				new Uint8Array(recipientData?.signedEncryptionKey!),
-			);
-		expect(isValidEncryptionKey).toBeTruthy();
-		await transferEncryptedUserShare(
-			dwalletSenderToolbox.client,
-			dwalletSenderToolbox.keypair,
-			encryptedUserShareAndProof,
-			publicKeyID,
-			dwallet!,
-		);
-
-		const decryptedKeyShare = decrypt_user_share(
-			encryptionKey,
-			decryptionKey,
-			encryptedUserShareAndProof,
-		);
-		expect(decryptedKeyShare).toEqual(new Uint8Array(secretShare));
-
-		expect(
-			verify_user_share(
-				new Uint8Array(decryptedKeyShare),
-				new Uint8Array(dwallet?.decentralizedDKGOutput!),
-			),
-		).toBeTruthy();
 	});
 
 	it('creates an encryption key & stores it in the active encryption keys table', async () => {
@@ -125,6 +61,7 @@ describe('Secret key share transfer', () => {
 
 	it('full flow - encrypts a secret share to a given Sui public key successfully, and store it on chain from the receiving end', async () => {
 		// ======================= Create Source DWallet =======================
+		// TODO (#202): Create a function that retrieves an encryption key for the given keypair if it exists
 		const [walletCreatorEncryptionKey, walletCreatorDecryptionKey] =
 			generatePaillierKeyPairFromSuiKeyPair(dwalletSenderToolbox.keypair);
 
@@ -171,6 +108,7 @@ describe('Secret key share transfer', () => {
 			createdDwallet?.encryptedSecretShareObjId!,
 		);
 		// Verify I signed the dkg output public keys before using it to send the user share.
+		let signedDWalletPubKeys = new Uint8Array(encryptedSecretShare?.signedDWalletPubkeys!);
 		expect(
 			await dwalletSenderToolbox.keypair
 				.getPublicKey()
@@ -178,7 +116,7 @@ describe('Secret key share transfer', () => {
 					serialized_pubkeys_from_decentralized_dkg_output(
 						new Uint8Array(createdDwallet?.decentralizedDKGOutput!),
 					),
-					new Uint8Array(encryptedSecretShare?.signedDWalletPubkeys!),
+					signedDWalletPubKeys,
 				),
 		).toBeTruthy();
 
@@ -188,6 +126,7 @@ describe('Secret key share transfer', () => {
 			createdDwallet!,
 			dwalletReceiverToolbox.keypair.getPublicKey(),
 			encryptionKeysHolder.objectId,
+			signedDWalletPubKeys,
 		);
 
 		// ======================= Verify Received DWallet is Valid =======================
@@ -197,36 +136,16 @@ describe('Secret key share transfer', () => {
 			encryptedUserShareObjID!,
 		);
 		expect(
-			await verifyEncryptedSecretShare(
+			await acceptUserShare(
 				encryptedUserShare!,
 				dwalletSenderToolbox.keypair.toSuiAddress(),
 				walletReceiverEncryptionKey,
 				walletReceiverDecryptionKey,
 				createdDwallet?.dwalletId!,
+				encryptionKeysHolder.objectId,
 				dwalletReceiverToolbox.client,
+				dwalletReceiverToolbox.keypair,
 			),
 		).toBeTruthy();
-
-		// ======================= Receiver Encrypts Secret Share To Himself =======================
-		const decryptedKeyShare = decrypt_user_share(
-			walletReceiverEncryptionKey,
-			walletReceiverDecryptionKey,
-			new Uint8Array(encryptedUserShare?.encryptedUserShareAndProof!),
-		);
-		let dwalletToSend = {
-			dwalletId: createdDwallet!.dwalletId,
-			secretKeyShare: Array.from(decryptedKeyShare),
-			decentralizedDKGOutput: createdDwallet!.decentralizedDKGOutput,
-		};
-
-		// It is now safe to sign the dwallet public keys, as we verified in verifyEncryptedSecretShare that they have been signed by the desired
-		// Sui source address.
-		await sendUserShareToSuiPubKey(
-			dwalletReceiverToolbox.client,
-			dwalletReceiverToolbox.keypair,
-			dwalletToSend,
-			dwalletReceiverToolbox.keypair.getPublicKey(),
-			encryptionKeysHolder.objectId,
-		);
 	});
 });
