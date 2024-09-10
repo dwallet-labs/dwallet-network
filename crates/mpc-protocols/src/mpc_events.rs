486f15a50640bf8dc0cdc59714cc91e23ea7bd4d
Yael Abergel <yaelab200@gmail.com>
2024-09-10 13:11:32 +0300

    added test

--- crates/mpc-protocols/src/mpc_events.rs
+++ crates/mpc-protocols/src/mpc_events.rs
@@ -7,7 +7,7 @@ use sui_types::{
 
 // Rust version of the Move sui_system::dwallet::CreatedProofMPCSessionEvent type
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema, Eq, PartialEq)]
-pub struct CreatedProofMPCSessionEvent<E> {
+pub struct CreatedProofMPCSessionEvent {
     pub session_id: ID,
     pub sender: SuiAddress,
 }
