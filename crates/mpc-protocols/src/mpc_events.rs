07487aacf3711441c784bc81bdf8538b7aaa8ad9
Yael Abergel <yaelab200@gmail.com>
2024-09-10 12:52:02 +0300

    fmt

--- crates/mpc-protocols/src/mpc_events.rs
+++ crates/mpc-protocols/src/mpc_events.rs
@@ -1,9 +1,9 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 use sui_types::{
     base_types::{ObjectID, SuiAddress},
     id::ID,
 };
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
 // Rust version of the Move sui_system::dwallet::CreatedProofMPCSessionEvent type
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema, Eq, PartialEq)]
