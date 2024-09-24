d9ed452646f5f7f0fe8d4c33c9a26c73ba081c03
Itay <itaylevy134@gmail.com>
2024-09-24 13:19:52 +0300

    code compiles

--- crates/mpc-protocols/src/mpc_service.rs
+++ crates/mpc-protocols/src/mpc_service.rs
@@ -1,5 +1,5 @@
 use crate::mpc_events::{CreatedProofMPCEvent, MPCEvent};
-use log::info;
+use log::{debug, info};
 use pera_types::base_types::ObjectID;
 use pera_types::event::Event;
 use std::cell::RefCell;
@@ -19,6 +19,42 @@ struct MPCInstance {
     /// The channel to send message to this instance's message handler thread when this instance is active
     input_receiver: Option<mpsc::Sender<MPCInput>>,
     pending_messages: Vec<MPCInput>,
+}
+
+impl MPCInstance {
+    fn set_active(&mut self) {
+        self.status = MPCSessionStatus::Active;
+        // TODO (#256): Replace hard coded 100 with the number of validators times 10
+        let (messages_handler_sender, messages_handler_receiver) = mpsc::channel(100);
+        self.input_receiver = Some(messages_handler_sender);
+        self.spawn_mpc_messages_handler(messages_handler_receiver);
+    }
+
+    /// Spawns an asynchronous task to handle incoming messages.
+    /// The [`MPCService`] will forward any message related to that instance to this channel.
+    fn spawn_mpc_messages_handler(&self, mut receiver: mpsc::Receiver<MPCInput>) {
+        tokio::spawn(async move {
+            while let Some(message) = receiver.recv().await {
+                // TODO (#235): Implement MPC messages handling
+            }
+        });
+    }
+
+    fn handle_message(&mut self, message: MPCInput) {
+        match self.status {
+            MPCSessionStatus::Active => {
+                if let Some(input_receiver) = &self.input_receiver {
+                    let _ = input_receiver.send(message);
+                }
+            }
+            MPCSessionStatus::Pending => {
+                self.pending_messages.push(message);
+            }
+            MPCSessionStatus::Finished => {
+                // Do nothing
+            }
+        }
+    }
 }
 
 /// Possible status of an MPC session
@@ -54,15 +90,6 @@ impl MPCService {
         }
     }
 
-    /// Spawns an asynchronous task to handle incoming messages for a new MPC instance.
-    /// The [`MPCService`] will forward any message related to that instance to this channel.
-    fn spawn_mpc_messages_handler(&self, mut receiver: mpsc::Receiver<MPCInput>) {
-        tokio::spawn(async move {
-            while let Some(message) = receiver.recv().await {
-                // TODO (#235): Implement MPC messages handling
-            }
-        });
-    }
 
     /// Filter the relevant MPC events from the transaction events & handle them
     pub fn handle_mpc_events(&mut self, events: &Vec<Event>) -> anyhow::Result<()> {
@@ -79,41 +106,74 @@ impl MPCService {
     /// Handles a proof initialization event
     /// Spawns a new MPC instance if the number of active instances is below the limit
     /// Otherwise, adds the instance to the pending queue
+    /// Handles a proof initialization event
+    /// Spawns a new MPC instance if the number of active instances is below the limit
+    /// Otherwise, adds the instance to the pending queue
     fn handle_proof_init_event(&mut self, event: CreatedProofMPCEvent) {
         info!(
             "Received start flow event for session ID {:?}",
             event.session_id
         );
-        // If the number of active instances exceeds the limit, add to pending
-        if self.active_instances_counter >= MAX_ACTIVE_MPC_INSTANCES {
-            self.mpc_instances.insert(
-                event.session_id.clone().bytes,
-                MPCInstance {
-                    status: MPCSessionStatus::Pending,
-                    input_receiver: None,
-                    pending_messages: vec![],
-                },
-            );
+
+        let mut new_instance = MPCInstance {
+            status: MPCSessionStatus::Pending,
+            input_receiver: None,
+            pending_messages: vec![],
+        };
+
+        // Activate the instance if possible
+        if self.active_instances_counter < MAX_ACTIVE_MPC_INSTANCES {
+            new_instance.set_active();
+            new_instance.handle_message(MPCInput::InitEvent(event.clone()));
+            self.active_instances_counter += 1;
+        } else {
             self.pending_instances_queue
                 .push_back(event.session_id.bytes);
-            return;
-        }
-        // TODO (#256): Replace hard coded 100 with the number of validators times 10
-        let (messages_handler_sender, messages_handler_receiver) = mpsc::channel(100);
-        self.spawn_mpc_messages_handler(messages_handler_receiver);
-        let _ = messages_handler_sender.send(MPCInput::InitEvent(event.clone()));
-        self.mpc_instances.insert(
-            event.session_id.clone().bytes,
-            MPCInstance {
-                status: MPCSessionStatus::Active,
-                input_receiver: Some(messages_handler_sender),
-                pending_messages: vec![],
-            },
-        );
-        self.active_instances_counter += 1;
+        };
+
+        self.mpc_instances
+            .insert(event.session_id.clone().bytes, new_instance);
+
         info!(
             "Added MPCInstance to service for session_id {:?}",
             event.session_id
         );
     }
+    // fn handle_proof_init_event(&mut self, event: CreatedProofMPCEvent) {
+    //     info!(
+    //         "Received start flow event for session ID {:?}",
+    //         event.session_id
+    //     );
+    //     // If the number of active instances exceeds the limit, add to pending
+    //     if self.active_instances_counter >= MAX_ACTIVE_MPC_INSTANCES {
+    //         self.mpc_instances.insert(
+    //             event.session_id.clone().bytes,
+    //             MPCInstance {
+    //                 status: MPCSessionStatus::Pending,
+    //                 input_receiver: None,
+    //                 pending_messages: vec![],
+    //             },
+    //         );
+    //         self.pending_instances_queue
+    //             .push_back(event.session_id.bytes);
+    //         return;
+    //     }
+    //     // TODO (#256): Replace hard coded 100 with the number of validators times 10
+    //     let (messages_handler_sender, messages_handler_receiver) = mpsc::channel(100);
+    //     self.spawn_mpc_messages_handler(messages_handler_receiver);
+    //     let _ = messages_handler_sender.send(MPCInput::InitEvent(event.clone()));
+    //     self.mpc_instances.insert(
+    //         event.session_id.clone().bytes,
+    //         MPCInstance {
+    //             status: MPCSessionStatus::Active,
+    //             input_receiver: Some(messages_handler_sender),
+    //             pending_messages: vec![],
+    //         },
+    //     );
+    //     self.active_instances_counter += 1;
+    //     info!(
+    //         "Added MPCInstance to service for session_id {:?}",
+    //         event.session_id
+    //     );
+    // }
 }
