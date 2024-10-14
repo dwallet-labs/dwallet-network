1138c77cc132402b15e1de5977c72eb47e635751
Itay <itaylevy134@gmail.com>
2024-10-14 16:36:16 +0300

    wasm code compiles

--- sdk/signature-mpc-wasm/src/lib.rs
+++ sdk/signature-mpc-wasm/src/lib.rs
@@ -1,9 +1,35 @@
 use wasm_bindgen::JsValue;
-
+use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
 // Copyright (c) dWallet Labs, Ltd.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 #[wasm_bindgen]
 pub fn hello_wasm() -> Result<JsValue, JsErr> {
     println!("Hello, wasm!");
-    Ok(JsValue::from_serde(&true).unwrap())
+    Ok(JsValue::from_bool(true))
+}
+
+
+impl<T: std::error::Error> From<T> for JsErr {
+    fn from(err: T) -> Self {
+        JsErr {
+            display: format!("{}", err),
+            message: err.to_string(),
+        }
+    }
+}
+
+impl From<JsErr> for JsValue {
+    fn from(err: JsErr) -> Self {
+        serde_wasm_bindgen::to_value(&err).unwrap()
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+/// Error type for better JS handling and generalization
+/// of Rust / WASM -> JS error conversion.
+pub struct JsErr {
+    // type_: String,
+    message: String,
+    display: String,
 }
