74802a7576203ac9a4a079ff9e120c5b8c4c3e58
Itay <itaylevy134@gmail.com>
2024-10-14 15:47:55 +0300

    fetching first round output from ts successfully

--- sdk/typescript/src/signature-mpc/proof.ts
+++ sdk/typescript/src/signature-mpc/proof.ts
@@ -35,11 +35,58 @@ export async function launchDKGSession(keypair: Keypair, client: PeraClient) {
 		arguments: [],
 	});
 
-	await client.signAndExecuteTransaction({
+	const result = await client.signAndExecuteTransaction({
 		signer: keypair,
 		transaction: tx,
 		options: {
 			showEffects: true,
 		},
 	});
+	const sessionRef = result.effects?.created?.filter((o) => o.owner === 'Immutable')[0].reference!;
+
+	// sleep five seconds
+	await new Promise((resolve) => setTimeout(resolve, 5000));
+	let firstRoundOutput = await fetchObjectBySessionId(
+		sessionRef.objectId,
+		`${packageId}::${dwalletModuleName}::CompletedFirstDKGRoundData`,
+		keypair,
+		client,
+	);
+	console.log({ firstRoundOutput });
+}
+
+export async function fetchObjectBySessionId(
+	sessionId: string,
+	type: string,
+	keypair: Keypair,
+	client: PeraClient,
+) {
+	let cursor = null;
+	for (;;) {
+		const objects = await client.getOwnedObjects({
+			owner: keypair.toPeraAddress(),
+			cursor: cursor,
+		});
+		const objectsContent = await client.multiGetObjects({
+			ids: objects.data.map((o) => o.data?.objectId!),
+			options: { showContent: true },
+		});
+
+		const objectsFiltered = objectsContent
+			.map((o) => o.data?.content)
+			.filter((o) => {
+				return (
+					// @ts-ignore
+					o?.dataType === 'moveObject' && o?.type === type && o.fields['session_id'] === sessionId
+				);
+			});
+		if (objectsFiltered.length > 0) {
+			return objectsFiltered[0];
+		} else if (objects.hasNextPage) {
+			cursor = objects.nextCursor;
+		} else {
+			cursor = null;
+		}
+		await new Promise((r) => setTimeout(r, 500));
+	}
 }
