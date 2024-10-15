249f1e830e5ff714ac0ca14639f5a6712bb28507
Yael Abergel <yaelab200@gmail.com>
2024-10-15 13:53:21 +0300

    compiles

--- crates/dwallet-mpc/src/lib.rs
+++ crates/dwallet-mpc/src/lib.rs
@@ -1,20 +1,22 @@
+use group::secp256k1;
 use mpc::two_party::Round;
-use pera_core::signature_mpc::dkg::{setup_paillier_secp256k1, AsyncProtocol};
-use rand_core::{CryptoRngCore, OsRng};
+use rand_core::OsRng;
 use std::marker::PhantomData;
-use twopc_mpc::dkg::decentralized_party::encryption_of_secret_key_share_round::AuxiliaryInput;
+use tiresias::test_helpers::N;
+use twopc_mpc::secp256k1::paillier::bulletproofs::PaillierProtocolPublicParameters;
 
+type AsyncProtocol = twopc_mpc::secp256k1::paillier::bulletproofs::AsyncProtocol<PhantomData<()>>;
 type DKGCentralizedParty = <AsyncProtocol as twopc_mpc::dkg::Protocol>::DKGCentralizedParty;
+
 pub fn create_centralized_output(first_round_output: Vec<u8>) -> anyhow::Result<Vec<u8>> {
     let first_round_output: <AsyncProtocol as twopc_mpc::dkg::Protocol>::EncryptionOfSecretKeyShareAndPublicKeyShare = bcs::from_bytes(&first_round_output)?;
 
-    let (secp256k1_group_public_parameters, _) = setup_paillier_secp256k1();
-
+    let secp256k1_group_public_parameters =
+        PaillierProtocolPublicParameters::new::<secp256k1::GroupElement>(N);
     let auxiliary_input = (secp256k1_group_public_parameters, PhantomData).into();
 
     let (outgoing_message, _) =
         DKGCentralizedParty::advance(first_round_output, &auxiliary_input, &mut OsRng)?;
-    // let output = bcs::to_bytes(&output)?;
     let outgoing_message = bcs::to_bytes(&outgoing_message)?;
     Ok(outgoing_message)
 }
