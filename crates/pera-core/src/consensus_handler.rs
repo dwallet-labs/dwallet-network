048aaf07137fe9ad3a646fb30de60ee1cfc38ded
Itay <itaylevy134@gmail.com>
2024-10-29 12:05:53 +0200

    added todo

--- crates/pera-core/src/consensus_handler.rs
+++ crates/pera-core/src/consensus_handler.rs
@@ -410,6 +410,8 @@ impl<C: CheckpointServiceNotify + Send + Sync> ConsensusHandler<C> {
                                 SequencedConsensusTransactionKind::System(transaction),
                                 consensus_output.leader_author_index(),
                             ));
+                        } else {
+                            // TODO (#263): Mark and punish the malicious validator that sent an invalid output
                         }
                     }
 
