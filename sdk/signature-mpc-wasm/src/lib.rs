ca99f8c3eb58c6730ee9e0debdfce7a50096f721
Yael Abergel <yaelab200@gmail.com>
2024-11-03 16:34:37 +0200

    bla

--- sdk/signature-mpc-wasm/src/lib.rs
+++ sdk/signature-mpc-wasm/src/lib.rs
@@ -9,17 +9,26 @@ use dwallet_mpc::create_dkg_output;
 use log::debug;
 
 #[wasm_bindgen]
-pub fn hello_wasm(dkg_first_round_output: Vec<u8>, session_id: String) -> Result<Vec<u8>, JsErr> {
-    console_error_panic_hook::set_once();
-    console_log::init_with_level(log::Level::Debug).unwrap();
-    debug!("hello wasm {:?}", dkg_first_round_output);
-    let output = match create_dkg_output(dkg_first_round_output, session_id) {
-        Ok((output, _)) => output,
-        Err(e) => {debug!("{:?}", e);return Ok(vec![1,2]);},
-    };
-    Ok(output)
+pub fn create_dkg_centralized_output(dkg_first_round_output: Vec<u8>, session_id: String) -> Result<JsValue, JsErr> {
+    let (public_key_share_and_proof, centralized_output) = create_dkg_output(dkg_first_round_output, session_id).map_err(to_js_err)?;
+    // Ok(serde_wasm_bindgen::to_value(&(public_key_share_and_proof, centralized_output)).map_err(to_js_err)?)
+Ok(serde_wasm_bindgen::to_value(&(public_key_share_and_proof, centralized_output)).unwrap())
 }
 
+impl From<JsErr> for JsValue {
+    fn from(err: JsErr) -> Self {
+        serde_wasm_bindgen::to_value(&err).unwrap()
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+/// Error type for better JS handling and generalization
+/// of Rust / WASM -> JS error conversion.
+pub struct JsErr {
+    // type_: String,
+    message: String,
+    display: String,
+}
 
 // impl<T: std::error::Error> From<T> for JsErr {
 //     fn from(err: T) -> Self {
@@ -30,20 +39,11 @@ pub fn hello_wasm(dkg_first_round_output: Vec<u8>, session_id: String) -> Result
 //     }
 // }
 
-impl From<JsErr> for JsValue {
-    fn from(err: JsErr) -> Self {
-        serde_wasm_bindgen::to_value(&err).unwrap()
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-/// Error type for better JS handling and generalization
-/// of Rust / WASM -> JS error conversion.
-pub struct JsErr {
-    // type_: String,
-    message: String,
-    display: String,
-}
+// impl From<JsErr> for JsValue {
+//     fn from(err: JsErr) -> Self {
+//         serde_wasm_bindgen::to_value(&err).unwrap()
+//     }
+// }
 
 // There is no way to implement From<anyhow::Error> for JsErr
 // since the current From<Error> is generic, and it results in a conflict.
