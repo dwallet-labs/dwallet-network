5db3550a96596cb07698d85ec201b8be4893d820
Itay <itaylevy134@gmail.com>
2024-11-04 17:05:44 +0200

    undo redundant changes

--- crates/pera-core/src/execution_cache/cache_types.rs
+++ crates/pera-core/src/execution_cache/cache_types.rs
@@ -113,6 +113,12 @@ impl<I: Iterator> AssertOrdered<I> {
     }
 }
 
+impl<I: IntoIterator> From<I> for AssertOrdered<I::IntoIter> {
+    fn from(iter: I) -> Self {
+        Self::new(iter.into_iter())
+    }
+}
+
 impl<I: Iterator> Iterator for AssertOrdered<I>
 where
     I::Item: Ord + Copy,
