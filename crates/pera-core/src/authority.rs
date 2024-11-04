4ad76b2052429dc0c5cfec8842966e74cefd44d9
Itay <itaylevy134@gmail.com>
2024-11-04 15:26:22 +0200

    removed redundant print

--- crates/pera-core/src/authority.rs
+++ crates/pera-core/src/authority.rs
@@ -1567,7 +1567,6 @@ impl AuthorityState {
                     let deserialized_event: CompletedDKGSecondRoundEvent =
                         bcs::from_bytes(&event.contents)?;
                     bytes_mpc_manager.finalize_mpc_instance(deserialized_event.session_id.bytes)?;
-                    println!("created dwallet {:?}", deserialized_event.dwallet_id);
                 } else {
                     match MPCParty::from_event(
                         event,
