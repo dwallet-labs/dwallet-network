a27276aae74e4e0c8d3746ff4e13c3229184b24d
Itay <itaylevy134@gmail.com>
2024-12-09 10:53:55 +0200

    another fix

--- pera-execution/v1/pera-adapter/src/execution_engine.rs
+++ pera-execution/v1/pera-adapter/src/execution_engine.rs
@@ -671,7 +671,9 @@ mod checked {
             TransactionKind::DWalletMPCOutput(_) => {
                 panic!("DWalletMPCOutput should not exist in v1");
             }
-            TransactionKind::LockNextCommittee(..) => Ok(Mode::empty_results()),
+            TransactionKind::LockNextCommittee(..) => {
+                panic!("LockNextCommittee should not exist in v1");
+            }
         }?;
         temporary_store.check_execution_results_consistency()?;
         Ok(result)
