805db5126327e5bdd37916d7382f48ed04883aba
Itay <itaylevy134@gmail.com>
2024-12-09 14:33:19 +0200

    more changes

--- crates/dwallet-mpc-centralized-party/src/lib.rs
+++ crates/dwallet-mpc-centralized-party/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 /// This crate contains the cryptographic logic for the centralized 2PC-MPC party
 use k256::ecdsa::hazmat::bits2field;
 use k256::ecdsa::signature::digest::{Digest, FixedOutput};
@@ -10,23 +11,39 @@ use twopc_mpc::secp256k1;
 type AsyncProtocol = twopc_mpc::secp256k1::class_groups::AsyncProtocol;
 type DKGCentralizedParty = <AsyncProtocol as twopc_mpc::dkg::Protocol>::DKGCentralizedParty;
 type SignCentralizedParty = <AsyncProtocol as twopc_mpc::sign::Protocol>::SignCentralizedParty;
+type EncryptionOfSecretKeyShareAndPublicKeyShare =
+    <AsyncProtocol as twopc_mpc::dkg::Protocol>::EncryptionOfSecretKeyShareAndPublicKeyShare;
+/// Supported hash functions for message digest
+#[derive(Clone, Debug)]
+pub enum Hash {
+    KECCAK256 = 0,
+    SHA256 = 1,
+}
 
 /// Executes the second phase of the DKG protocol, part of a three-phase DKG flow.
 ///
-/// The [`create_dkg_output`] function is called by the client (aka the centralized party)
-/// and is responsible for generating and returning the public key share and its proof, as well as the
-/// centralized DKG output. These values are necessary for the decentralized party to complete the final
-/// phase of the DKG protocol.
+/// This function is invoked by the centralized party to produce:
+/// - A public key share and its proof.
+/// - Centralized DKG output required for further protocol steps.
 ///
-/// * `decentralized_first_round_output` - A serialized byte vector representing the output of the
-///   decentralized party from the first round.
-/// * `session_id` - A unique identifier for the session, represented as a hexadecimal string.
-///   Received from the `pera_system::dwallet_2pc_mpc_ecdsa_k1::launch_dkg_first_round` transaction.
+/// # Parameters
+/// — `decentralized_first_round_output`:
+///    Serialized output of the decentralized party from the first DKG round.
+/// — `session_id`: Unique hexadecimal string identifying the session.
+///
+/// # Returns
+/// A tuple containing:
+/// - Serialized public key share and proof.
+/// - Serialized centralized DKG output.
+///
+/// # Errors
+/// Returns an error if decoding or advancing the protocol fails.
 pub fn create_dkg_output(
     decentralized_first_round_output: Vec<u8>,
     session_id: String,
 ) -> anyhow::Result<(Vec<u8>, Vec<u8>)> {
-    let decentralized_first_round_output: <AsyncProtocol as twopc_mpc::dkg::Protocol>::EncryptionOfSecretKeyShareAndPublicKeyShare = bcs::from_bytes(&decentralized_first_round_output)?;
+    let decentralized_first_round_output: EncryptionOfSecretKeyShareAndPublicKeyShare =
+        bcs::from_bytes(&decentralized_first_round_output)?;
     let public_parameters = class_groups_constants::protocol_public_parameters();
     let session_id = commitment::CommitmentSizedNumber::from_le_hex(&session_id);
 
@@ -34,19 +51,13 @@ pub fn create_dkg_output(
         decentralized_first_round_output,
         &(public_parameters, session_id).into(),
         &mut OsRng,
-    )?;
+    )
+    .context("advance() failed on the DKGCentralizedParty")?;
 
     let public_key_share_and_proof = bcs::to_bytes(&public_key_share_and_proof)?;
     let centralized_output = bcs::to_bytes(&centralized_output)?;
 
     Ok((public_key_share_and_proof, centralized_output))
-}
-
-/// Supported hash functions for message digest
-#[derive(Clone, Debug)]
-pub enum Hash {
-    KECCAK256 = 0,
-    SHA256 = 1,
 }
 
 impl TryFrom<u8> for Hash {
