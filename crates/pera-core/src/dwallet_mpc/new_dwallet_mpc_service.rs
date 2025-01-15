287b7b9eefb638b96a7dc20029b71cb8bbea232e
Itay <itaylevy134@gmail.com>
2025-01-15 18:17:23 +0200

    reading all the messages every 5 seconds

--- crates/pera-core/src/dwallet_mpc/new_dwallet_mpc_service.rs
+++ crates/pera-core/src/dwallet_mpc/new_dwallet_mpc_service.rs
@@ -1,9 +1,9 @@
-use std::sync::{Arc, Weak};
+use crate::authority::authority_per_epoch_store::AuthorityPerEpochStore;
 use narwhal_types::Round;
 use pera_types::base_types::EpochId;
 use pera_types::dwallet_mpc_error::{DwalletMPCError, DwalletMPCResult};
+use std::sync::{Arc, Weak};
 use typed_store::Map;
-use crate::authority::authority_per_epoch_store::AuthorityPerEpochStore;
 
 struct NewDWalletMPCService {
     last_read_narwhal_round: Round,
@@ -22,20 +22,22 @@ impl NewDWalletMPCService {
         }
     }
 
-    pub fn spawn(&self) {
-        let new_messages = self
-            .epoch_store().unwrap()
-            .tables().unwrap()
-            .dwallet_mpc_messages
-            .iter_with_bounds(
-                Some(self.last_read_narwhal_round),
-                None,
-            )
-            .map(|(_, messages)| messages)
-            .flatten()
-            .collect();
-
-        // Update the MPC manager with the new messages.
+    pub async fn spawn(&mut self) {
+        loop {
+            let new_dwallet_messages_iter = self
+                .epoch_store()
+                .unwrap()
+                .tables()
+                .unwrap()
+                .dwallet_mpc_messages
+                .iter_with_bounds(Some(self.last_read_narwhal_round), None);
+            let mut new_messages = vec![];
+            for (round, messages) in new_dwallet_messages_iter {
+                self.last_read_narwhal_round = round;
+                new_messages.extend(messages);
+            }
+            tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+        }
     }
 
     fn epoch_store(&self) -> DwalletMPCResult<Arc<AuthorityPerEpochStore>> {
