77f787984f88637b23d4949c8256235c59929426
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-01-28 15:28:40 +0200

    wip

--- crates/dwallet-mpc-types/src/dwallet_mpc.rs
+++ crates/dwallet-mpc-types/src/dwallet_mpc.rs
@@ -70,9 +70,7 @@ impl fmt::Display for MPCSessionStatus {
         match self {
             MPCSessionStatus::Pending => write!(f, "Pending"),
             MPCSessionStatus::Active => write!(f, "Active"),
-            MPCSessionStatus::Finished => {
-                write!(f, "Finished")
-            }
+            MPCSessionStatus::Finished => write!(f, "Finished"),
             MPCSessionStatus::Failed => write!(f, "Failed"),
         }
     }
