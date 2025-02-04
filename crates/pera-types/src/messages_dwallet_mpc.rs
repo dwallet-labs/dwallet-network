a0da2c8e9e4cf4a469f42e160266799dd8cf89c7
Itay <itaylevy134@gmail.com>
2025-02-04 12:00:10 +0200

    fix

--- crates/pera-types/src/messages_dwallet_mpc.rs
+++ crates/pera-types/src/messages_dwallet_mpc.rs
@@ -182,12 +182,12 @@ pub struct StartEncryptedShareVerificationEvent {
     /// Encrypted centralized secret key share and the associated
     /// cryptographic proof of encryption.
     pub encrypted_centralized_secret_share_and_proof: Vec<u8>,
-    /// The public output of the centralized party,
-    /// belongs to the dWallet that its centralized secret share is being encrypted.
-    pub decentralized_public_output: Vec<u8>,
     /// The signature of the dWallet `centralized_public_output`,
     /// signed by the secret key that corresponds to `encryptor_ed25519_pubkey`.
     pub decentralized_public_output_signature: Vec<u8>,
+    /// The public output of the centralized party,
+    /// belongs to the dWallet that its centralized secret share is being encrypted.
+    pub decentralized_public_output: Vec<u8>,
     /// The ID of the dWallet that this encrypted secret key share belongs to.
     pub dwallet_id: ID,
     /// The encryption key used to encrypt the secret key share with.
