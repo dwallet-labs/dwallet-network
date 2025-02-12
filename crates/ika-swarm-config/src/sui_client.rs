67e63631c42e5f2aebff79e39e1e6708a750f2ca
Itay <itaylevy134@gmail.com>
2025-02-12 15:30:04 +0200

    write should work

--- crates/ika-swarm-config/src/sui_client.rs
+++ crates/ika-swarm-config/src/sui_client.rs
@@ -20,6 +20,7 @@ use shared_crypto::intent::Intent;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
+use serde::Serialize;
 use sui::client_commands::{
     estimate_gas_budget_from_gas_cost, execute_dry_run, max_gas_budget, request_tokens_from_faucet,
     SuiClientCommandResult,
