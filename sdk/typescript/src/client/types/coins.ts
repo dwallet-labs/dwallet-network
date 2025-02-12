51089fb9b97a845a846ce267d06142a423950d79
Itay <itaylevy134@gmail.com>
2025-02-12 14:25:55 +0200

    removed redundant code

--- sdk/typescript/src/client/types/coins.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-
-export type CoinBalance = {
-	coinType: string;
-	coinObjectCount: number;
-	totalBalance: string;
-	lockedBalance: Record<string, string>;
-};
--- sdk/typescript/src/keypairs/secp256r1/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-
-export {
-	DEFAULT_SECP256R1_DERIVATION_PATH,
-	Secp256r1Keypair,
-	type Secp256r1KeypairData,
-} from './keypair.js';
-export { Secp256r1PublicKey } from './publickey.js';
