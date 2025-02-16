49e221b1d5fe750ae5936131494158c0d9425919
Itay <itaylevy134@gmail.com>
2025-02-16 18:35:19 +0200

    removeunwrap

--- crates/ika-core/src/authority/authority_perpetual_tables.rs
+++ crates/ika-core/src/authority/authority_perpetual_tables.rs
@@ -93,17 +93,17 @@ impl AuthorityPerpetualTables {
         if let Some(cursor) = cursor {
             let mut batch = self.pending_events.batch();
             batch.insert_batch(&self.sui_syncer_cursors, [(module, cursor)])?;
-            batch.insert_batch(
-                &self.pending_events,
-                events.iter().map(|e| {
+            let serialized_events: Vec<Vec<u8>> = events
+                .iter()
+                .map(|e| {
                     let serialized_event = bcs::to_bytes(&DBSuiEvent {
                         type_: e.type_.clone(),
                         contents: e.bcs.clone().into_bytes(),
-                    })
-                    .unwrap();
-                    (e.id, serialized_event)
-                }),
-            )?;
+                    })?;
+                    Ok((e.id, serialized_event))
+                })
+                .collect()?;
+            batch.insert_batch(&self.pending_events, serialized_events)?;
             batch.write()?;
         }
         self.pending_events.rocksdb.flush()?;
