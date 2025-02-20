3a9fb53fc04abcf3888ad298af57f44ca8312211
Itay <itaylevy134@gmail.com>
2025-02-20 16:21:38 +0200

    order

--- sdk/typescript/src/dwallet-mpc/encrypt-user-share.ts
+++ sdk/typescript/src/dwallet-mpc/encrypt-user-share.ts
@@ -1,19 +1,17 @@
 import { generate_secp_cg_keypair_from_seed } from '@dwallet-network/dwallet-mpc-wasm';
-import { toHex, bcs } from "@mysten/bcs";
-import { Transaction } from "@mysten/sui/transactions";
-
-
+import { bcs, toHex } from '@mysten/bcs';
+import { Transaction } from '@mysten/sui/transactions';
 
 import {
 	checkpointCreationTime,
-	Config, delay,
+	Config,
+	delay,
 	DWALLET_ECDSAK1_INNER_MOVE_MODULE_NAME,
 	DWALLET_ECDSAK1_MOVE_MODULE_NAME,
 	fetchObjectWithType,
 	getDWalletSecpState,
 	getInitialSharedVersion,
 } from './globals';
-
 
 /**
  * A class groups key pair.
@@ -25,36 +23,12 @@ interface ClassGroupsSecpKeyPair {
 }
 
 /**
- * Retrieves the active encryption key object ID
- * from the active encryption keys table *activeEncryptionKeysTableID*
- * for the given address — derived from the public key.
- * Throws an error otherwise.
+ * Event emitted by the blockchain when an
+ * `EncryptionKey` Move object is created.
  */
-async function getActiveEncryptionKeyObjID(conf: Config, address: string): Promise<string> {
-	const tx = new Transaction();
-	let dwalletState = await getDWalletSecpState(conf);
-	tx.moveCall({
-		target: `${conf.ikaConfig.ika_system_package_id}::${DWALLET_ECDSAK1_MOVE_MODULE_NAME}::get_active_encryption_key`,
-		arguments: [
-			tx.sharedObjectRef({
-				objectId: dwalletState.object_id,
-				initialSharedVersion: dwalletState.initial_shared_version,
-				mutable: false,
-			}),
-			tx.pure.address(address)],
-	});
-
-	// Safe to use this function as it has been used here:
-	// https://github.com/dwallet-labs/dwallet-network/blob/29929ded135f05578b6ce33b52e6ff5e894d0487/sdk/deepbook-v3/src/client.ts#L84
-	// in late 2024 (can be seen with git blame).
-	// Note that regular `getObject()` is not working because of dynamic fields.
-	const res = await conf.client.devInspectTransactionBlock({
-		sender: address,
-		transactionBlock: tx,
-	});
-
-	const objIDArray = new Uint8Array(res.results?.at(0)?.returnValues?.at(0)?.at(0) as number[]);
-	return toHex(objIDArray);
+interface CreatedEncryptionKeyEvent {
+	session_id: string;
+	encryption_key_id: string;
 }
 
 /**
@@ -66,9 +40,14 @@ interface EncryptionKey {
 	encryption_key_signature: Uint8Array;
 }
 
-const isEncryptionKey = (obj: any): obj is EncryptionKey => {
+interface CreatedEncryptionKeyEvent {
+	encryption_key_id: string;
+	signer_address: string;
+}
+
+function isEncryptionKey(obj: any): obj is EncryptionKey {
 	return 'encryption_key' in obj && 'signer_address' in obj && 'encryption_key_signature' in obj;
-};
+}
 
 export async function getOrCreateClassGroupsKeyPair(conf: Config): Promise<ClassGroupsSecpKeyPair> {
 	const [expectedEncryptionKey, decryptionKey] = generate_secp_cg_keypair_from_seed(
@@ -101,7 +80,7 @@ export async function getOrCreateClassGroupsKeyPair(conf: Config): Promise<Class
 
 	const encryptionKeyCreationEvent: CreatedEncryptionKeyEvent = await registerEncryptionKey(
 		conf,
-		expectedEncryptionKey
+		expectedEncryptionKey,
 	);
 
 	return {
@@ -111,8 +90,42 @@ export async function getOrCreateClassGroupsKeyPair(conf: Config): Promise<Class
 	};
 }
 
-export enum EncryptionKeyScheme {
-	ClassGroups = 0,
+function isEqual(arr1: Uint8Array, arr2: Uint8Array): boolean {
+	return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+}
+
+/**
+ * Retrieves the active encryption key object ID
+ * from the active encryption keys table *activeEncryptionKeysTableID*
+ * for the given address — derived from the public key.
+ * Throws an error otherwise.
+ */
+async function getActiveEncryptionKeyObjID(conf: Config, address: string): Promise<string> {
+	const tx = new Transaction();
+	let dwalletState = await getDWalletSecpState(conf);
+	tx.moveCall({
+		target: `${conf.ikaConfig.ika_system_package_id}::${DWALLET_ECDSAK1_MOVE_MODULE_NAME}::get_active_encryption_key`,
+		arguments: [
+			tx.sharedObjectRef({
+				objectId: dwalletState.object_id,
+				initialSharedVersion: dwalletState.initial_shared_version,
+				mutable: false,
+			}),
+			tx.pure.address(address),
+		],
+	});
+
+	// Safe to use this function as it has been used here:
+	// https://github.com/dwallet-labs/dwallet-network/blob/29929ded135f05578b6ce33b52e6ff5e894d0487/sdk/deepbook-v3/src/client.ts#L84
+	// in late 2024 (can be seen with git blame).
+	// Note that regular `getObject()` is not working because of dynamic fields.
+	const res = await conf.client.devInspectTransactionBlock({
+		sender: address,
+		transactionBlock: tx,
+	});
+
+	const objIDArray = new Uint8Array(res.results?.at(0)?.returnValues?.at(0)?.at(0) as number[]);
+	return toHex(objIDArray);
 }
 
 /**
@@ -131,7 +144,7 @@ export enum EncryptionKeyScheme {
  */
 async function registerEncryptionKey(
 	conf: Config,
-	encryptionKey: Uint8Array
+	encryptionKey: Uint8Array,
 ): Promise<CreatedEncryptionKeyEvent> {
 	// Sign the encryption key with the key pair.
 	const encryptionKeySignature = await conf.keypair.sign(new Uint8Array(encryptionKey));
@@ -158,33 +171,15 @@ async function registerEncryptionKey(
 			showEvents: true,
 		},
 	});
-	let createdEncryptionKeyEvent = res.events?.find(event => isCreatedEncryptionKeyEvent(event.parsedJson));
+	let createdEncryptionKeyEvent = res.events?.find((event) =>
+		isCreatedEncryptionKeyEvent(event.parsedJson),
+	);
 	if (!createdEncryptionKeyEvent) {
 		throw new Error('Encryption key registration failed');
 	}
 	return createdEncryptionKeyEvent.parsedJson as CreatedEncryptionKeyEvent;
 }
 
-interface CreatedEncryptionKeyEvent{
-	encryption_key_id: string,
-	signer_address: string,
-}
-
 function isCreatedEncryptionKeyEvent(obj: any): obj is CreatedEncryptionKeyEvent {
 	return 'encryption_key_id' in obj && 'signer_address' in obj;
-}
-
-
-/**
- * Event emitted by the blockchain when an
- * `EncryptionKey` Move object is created.
- */
-interface CreatedEncryptionKeyEvent {
-	session_id: string;
-	encryption_key_id: string;
-}
-
-
-export function isEqual(arr1: Uint8Array, arr2: Uint8Array): boolean {
-	return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
