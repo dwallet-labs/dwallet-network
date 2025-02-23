91065ff06f524b892cce3cdff2e224e8d8141041
Itay <itaylevy134@gmail.com>
2025-02-23 15:38:23 +0200

    lint

--- sdk/typescript/src/dwallet-mpc/encrypt-user-share.ts
+++ sdk/typescript/src/dwallet-mpc/encrypt-user-share.ts
@@ -1,6 +1,5 @@
 import { generate_secp_cg_keypair_from_seed } from '@dwallet-network/dwallet-mpc-wasm';
 import { bcs, toHex } from '@mysten/bcs';
-import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import { Transaction } from '@mysten/sui/transactions';
 
 import type { Config } from './globals.js';
