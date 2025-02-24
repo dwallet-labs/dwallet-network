a55ee0671fcc244fcc68aead21b19c5e0febd859
Itay <itaylevy134@gmail.com>
2025-02-24 15:45:27 +0200

    remove

--- crates/ika-core/src/dwallet_mpc/encrypt_user_share.rs
+++ crates/ika-core/src/dwallet_mpc/encrypt_user_share.rs
@@ -66,8 +66,6 @@ fn verify_centralized_secret_key_share_proof(
     encryption_key: &Vec<u8>,
 ) -> anyhow::Result<()> {
     let protocol_public_params = protocol_public_parameters();
-    warn!("base 64 encryption key: {:?}", base64::encode(encryption_key));
-
     let language_public_parameters = construct_encryption_of_discrete_log_public_parameters::<
         SCALAR_LIMBS,
         { FUNDAMENTAL_DISCRIMINANT_LIMBS },
