28e0615355b62a47b838d59558a5188185a1365f
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-03-29 17:31:52 +0300

    wip

--- docker/ika-node/build.sh
+++ docker/ika-node/build.sh
@@ -11,6 +11,21 @@ DOCKERFILE="$DIR/Dockerfile"
 GIT_REVISION="$(git describe --always --abbrev=12 --dirty --exclude '*')"
 BUILD_DATE="$(date -u +'%Y-%m-%d')"
 
+# Load environment variables from .env
+if [ -f .env ]; then
+  export "$(grep -v '^#' .env | xargs)"
+else
+  echo ".env file not found!"
+  exit 1
+fi
+
+# Make sure GITHUB_TOKEN is set
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN is not set. Check your .env file."
+  exit 1
+fi
+
+
 # option to build using debug symbols
 if [ "$1" = "--debug-symbols" ]; then
 	PROFILE="bench"
@@ -32,4 +47,5 @@ docker build -f "$DOCKERFILE" "$REPO_ROOT" \
 	--build-arg GIT_REVISION="$GIT_REVISION" \
 	--build-arg BUILD_DATE="$BUILD_DATE" \
 	--build-arg PROFILE="$PROFILE" \
+	--build-arg GITHUB_TOKEN="$GITHUB_TOKEN" \
 	"$@"
