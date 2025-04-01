118dc64553274509062484f5f93904c2c3f62a57
Itay <itaylevy134@gmail.com>
2025-04-01 13:58:53 +0300

    fix

--- crates/ika-sui-client/src/lib.rs
+++ crates/ika-sui-client/src/lib.rs
@@ -754,9 +754,11 @@ impl SuiClientInner for SuiSdkClient {
 
                 // log the type with warn
                 warn!("received event with type {:?}", raw_move_obj.type_);
-
+                let TypeTag::Struct(event_tag) = raw_move_obj.type_.type_params.get(0).unwrap().clone() else {
+                    continue;
+                } ;
                 let event = DBSuiEvent {
-                    type_: raw_move_obj.type_,
+                    type_: *event_tag,
                     contents: raw_move_obj.bcs_bytes,
                 };
                 events.push(event);
