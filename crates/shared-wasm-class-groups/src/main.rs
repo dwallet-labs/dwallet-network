cf7d78705057dfdcbdf7b64611ca7673e37111f8
Itay <itaylevy134@gmail.com>
2025-04-07 15:39:23 +0300

    rm

--- crates/shared-wasm-class-groups/src/main.rs
+++
@@ -1,8 +0,0 @@
-use shared_wasm_class_groups::network_dkg_final_output;
-
-fn main() {
-    // This is a placeholder main function. The actual functionality is implemented in the modules.
-    println!("Hello, world!");
-    let mock = network_dkg_final_output();
-    println!("Mock: {:?}", mock);
-}
