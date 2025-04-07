a2de3d643479e3a4522e7a6e08e5989150e73770
Itay <itaylevy134@gmail.com>
2025-04-07 15:39:40 +0300

    undo

--- crates/shared-wasm-class-groups/tests/build_network_dkg_on_chain_output_test.rs
+++ crates/shared-wasm-class-groups/tests/build_network_dkg_on_chain_output_test.rs
@@ -1,4 +1,3 @@
-use class_groups::SecretKeyShareSizedInteger;
 use dwallet_mpc_types::dwallet_mpc::NetworkDecryptionKeyOnChainOutput;
 use group::{secp256k1, PartyID};
 use mpc::{Weight, WeightedThresholdAccessStructure};
@@ -13,21 +12,6 @@ fn build_network_dkg_on_chain_output_test() {
     let access_structure: HashMap<PartyID, Weight> =
         [(1, 1), (2, 1), (3, 1), (4, 1)].iter().cloned().collect();
     let access_structure = WeightedThresholdAccessStructure::new(3, access_structure).unwrap();
-
-    let decryption_key_share_public_parameters = network_decryption_key_public_output
-        .default_decryption_key_share_public_parameters::<secp256k1::GroupElement>(
-            &access_structure,
-        )
-        .unwrap();
-    let decryption_key_share_public_parameters_base64 =
-        base64::encode(bcs::to_bytes(&decryption_key_share_public_parameters).unwrap());
-    let decryption_key_share_public_parameters_path = "decryption_key_share_public_parameters.txt";
-    std::fs::write(
-        decryption_key_share_public_parameters_path,
-        decryption_key_share_public_parameters_base64,
-    )
-    .unwrap();
-
     let on_chain_output = NetworkDecryptionKeyOnChainOutput {
         encryption_key: bcs::to_bytes(&network_decryption_key_public_output.encryption_key)
             .unwrap(),
@@ -57,26 +41,5 @@ fn build_network_dkg_on_chain_output_test() {
 
     let bcs_on_chain_output = bcs::to_bytes(&on_chain_output).unwrap();
     let base64_on_chain_output = base64::encode(&bcs_on_chain_output);
-    std::fs::write(
-        "network_dkg_on_chain_output.txt",
-        base64_on_chain_output.clone(),
-    );
     println!("{}", base64_on_chain_output);
-}
-
-#[test]
-fn create_shares() {
-    let mut shares: HashMap<PartyID, HashMap<PartyID, SecretKeyShareSizedInteger>> = HashMap::new();
-    for i in 1..=4 {
-        let private_output_path = format!("/Users/yaelabergel/projects/dwallet_labs/dwallet-network-original/bas64_private_output_{}.txt", i);
-        let private_output = std::fs::read_to_string(private_output_path).unwrap();
-        let private_output = base64::decode(private_output).unwrap();
-        let private_output: HashMap<PartyID, SecretKeyShareSizedInteger> =
-            bcs::from_bytes(&private_output).unwrap();
-        shares.insert(i, private_output);
-    }
-    let shares = bcs::to_bytes(&shares).unwrap();
-    let base64_shares = base64::encode(&shares);
-    let shares_path = "decryption_key_shares.txt";
-    std::fs::write(shares_path, base64_shares).unwrap();
 }
