2844d1b0b157c04c8e3110b5f93cc42b0854de58
yaelAbergel <42145434+yaelAbergel@users.noreply.github.com>
2025-04-08 18:25:18 +0300

    chore: Generate the validator class groups key from random seed (#782)
    Co-authored-by: Zeev Manilovich <zeevmoney@users.noreply.github.com>
    Co-authored-by: Yehonatan Cohen Scaly <scalys7@gmail.com>

--- crates/ika-swarm-config/src/validator_initialization_config.rs
+++ crates/ika-swarm-config/src/validator_initialization_config.rs
@@ -4,7 +4,8 @@
 use std::net::{IpAddr, SocketAddr};
 
 use dwallet_classgroups_types::{
-    generate_class_groups_keypair_and_proof_from_seed, ClassGroupsKeyPairAndProof,
+    generate_class_groups_keypair_and_proof_from_seed, sample_seed,
+    write_class_groups_seed_to_file, ClassGroupsKeyPairAndProof,
 };
 use fastcrypto::traits::{KeyPair, ToFromBytes};
 use ika_config::local_ip_utils;
@@ -160,15 +161,8 @@ impl ValidatorInitializationConfigBuilder {
             .class_groups_key_pair_and_proof
             .clone()
             .unwrap_or_else(|| {
-                Box::new(generate_class_groups_keypair_and_proof_from_seed(
-                    protocol_key_pair
-                        .copy()
-                        .private()
-                        .as_bytes()
-                        .try_into()
-                        // Safe to unwrap because the key is 32 bytes.
-                        .unwrap(),
-                ))
+                let seed = sample_seed();
+                Box::new(generate_class_groups_keypair_and_proof_from_seed(seed))
             });
 
         let (consensus_key_pair, network_key_pair): (NetworkKeyPair, NetworkKeyPair) =
