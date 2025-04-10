778cf5f5fa42360deff7aace194e092c2f4f1945
Itay <itaylevy134@gmail.com>
2025-04-10 13:15:09 +0300

    working

--- crates/ika-core/src/checkpoints/checkpoint_output.rs
+++ crates/ika-core/src/checkpoints/checkpoint_output.rs
@@ -169,6 +169,9 @@ impl CheckpointOutput for LogCheckpointOutput {
         Ok(())
     }
 
+
+    // read clock sui all the time to get the current time when to run the mid epoch / lock next committee
+
     fn close_epoch(&self, epoch_store: &Arc<AuthorityPerEpochStore>) {
         info!("Closing epoch {}", epoch_store.epoch(),);
     }
