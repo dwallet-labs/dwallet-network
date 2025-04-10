209efd191fad82ff1cf68bd16267d64236bc5a73
Yael Abergel <yaelab200@gmail.com>
2025-04-10 13:43:44 +0300

    --wip-- [skip ci]

--- crates/ika/tests/some_test.rs
+++
@@ -1,6 +0,0 @@
-use dwallet_classgroups_types::read_class_groups_from_file;
-
-#[test]
-fn test_class_groups_deserialize() {
-    let _ = read_class_groups_from_file("/Users/yaelabergel/projects/dwallet_labs/dwallet-network-original/class-groups.key");
-}
