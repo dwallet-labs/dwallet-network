d8bd2eaba0ee88334bdd7fc4e93095749c93849c
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-04-16 13:32:16 +0300

    wip

--- crates/ika-core/src/sui_connector/sui_executor.rs
+++ crates/ika-core/src/sui_connector/sui_executor.rs
@@ -89,13 +89,13 @@ where
         ika_system_state_inner: &SystemInner,
     ) {
         let Ok(clock) = self.sui_client.get_clock().await else {
-            error!("Failed to get clock when running epoch switch");
+            error!("failed to get clock when running epoch switch");
             return;
         };
         let Some(dwallet_2pc_mpc_secp256k1_id) =
             ika_system_state_inner.dwallet_2pc_mpc_secp256k1_id()
         else {
-            error!("Failed to get dwallet_2pc_mpc_secp256k1_id when running epoch switch");
+            error!("failed to get `dwallet_2pc_mpc_secp256k1_id` when running epoch switch");
             return;
         };
         let SystemInner::V1(system_inner_v1) = &ika_system_state_inner;
@@ -123,7 +123,7 @@ where
             .get_dwallet_coordinator_inner(dwallet_2pc_mpc_secp256k1_id)
             .await
         else {
-            error!("Failed to get dwallet coordinator inner when running epoch switch");
+            error!("failed to get dwallet coordinator inner when running epoch switch");
             return;
         };
 
@@ -508,130 +508,3 @@ where
         Ok(())
     }
 }
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     use crate::{sui_client::SuiClient, sui_mock_client::SuiMockClient};
-//     use prometheus::Registry;
-//     use sui_json_rpc_types::EventPage;
-//     use sui_types::{digests::TransactionDigest, event::EventID, Identifier};
-//     use tokio::time::timeout;
-//
-//     #[tokio::test]
-//     async fn test_sui_syncer_basic() -> anyhow::Result<()> {
-//         telemetry_subscribers::init_for_testing();
-//         let registry = Registry::new();
-//         mysten_metrics::init_metrics(&registry);
-//         let metrics = Arc::new(SuiHandlerMetrics::new(&registry));
-//         let mock = SuiMockClient::default();
-//         let client = Arc::new(SuiClient::new_for_testing(mock.clone()));
-//         let module_foo = Identifier::new("Foo").unwrap();
-//         let module_bar = Identifier::new("Bar").unwrap();
-//         let empty_events = EventPage::empty();
-//         let cursor = EventID {
-//             tx_digest: TransactionDigest::random(),
-//             event_seq: 0,
-//         };
-//         add_event_response(&mock, module_foo.clone(), cursor, empty_events.clone());
-//         add_event_response(&mock, module_bar.clone(), cursor, empty_events.clone());
-//
-//         let target_modules = HashMap::from_iter(vec![
-//             (module_foo.clone(), Some(cursor)),
-//             (module_bar.clone(), Some(cursor)),
-//         ]);
-//         let interval = Duration::from_millis(200);
-//         let (_handles, mut events_rx) = SuiExecutor::new(client, target_modules, metrics.clone())
-//             .run(interval)
-//             .await
-//             .unwrap();
-//
-//         // Initially there are no events
-//         assert_no_more_events(interval, &mut events_rx).await;
-//
-//         mock.set_latest_checkpoint_sequence_number(999);
-//         // Module Foo has new events
-//         let mut event_1: SuiEvent = SuiEvent::random_for_testing();
-//         let package_id = BRIDGE_PACKAGE_ID;
-//         event_1.type_.address = package_id.into();
-//         event_1.type_.module = module_foo.clone();
-//         let module_foo_events_1: sui_json_rpc_types::Page<SuiEvent, EventID> = EventPage {
-//             data: vec![event_1.clone(), event_1.clone()],
-//             next_cursor: Some(event_1.id),
-//             has_next_page: false,
-//         };
-//         add_event_response(&mock, module_foo.clone(), event_1.id, empty_events.clone());
-//         add_event_response(
-//             &mock,
-//             module_foo.clone(),
-//             cursor,
-//             module_foo_events_1.clone(),
-//         );
-//
-//         let (identifier, received_events) = events_rx.recv().await.unwrap();
-//         assert_eq!(identifier, module_foo);
-//         assert_eq!(received_events.len(), 2);
-//         assert_eq!(received_events[0].id, event_1.id);
-//         assert_eq!(received_events[1].id, event_1.id);
-//         // No more
-//         assert_no_more_events(interval, &mut events_rx).await;
-//         assert_eq!(
-//             metrics
-//                 .last_synced_sui_checkpoints
-//                 .get_metric_with_label_values(&["Foo"])
-//                 .unwrap()
-//                 .get(),
-//             999
-//         );
-//
-//         // Module Bar has new events
-//         let mut event_2: SuiEvent = SuiEvent::random_for_testing();
-//         event_2.type_.address = package_id.into();
-//         event_2.type_.module = module_bar.clone();
-//         let module_bar_events_1 = EventPage {
-//             data: vec![event_2.clone()],
-//             next_cursor: Some(event_2.id),
-//             has_next_page: true, // Set to true so that the syncer will not update the last synced checkpoint
-//         };
-//         add_event_response(&mock, module_bar.clone(), event_2.id, empty_events.clone());
-//
-//         add_event_response(&mock, module_bar.clone(), cursor, module_bar_events_1);
-//
-//         let (identifier, received_events) = events_rx.recv().await.unwrap();
-//         assert_eq!(identifier, module_bar);
-//         assert_eq!(received_events.len(), 1);
-//         assert_eq!(received_events[0].id, event_2.id);
-//         // No more
-//         assert_no_more_events(interval, &mut events_rx).await;
-//         assert_eq!(
-//             metrics
-//                 .last_synced_sui_checkpoints
-//                 .get_metric_with_label_values(&["Bar"])
-//                 .unwrap()
-//                 .get(),
-//             0, // Not updated
-//         );
-//
-//         Ok(())
-//     }
-//
-//     async fn assert_no_more_events(
-//         interval: Duration,
-//         events_rx: &mut mysten_metrics::metered_channel::Receiver<(Identifier, Vec<SuiEvent>)>,
-//     ) {
-//         match timeout(interval * 2, events_rx.recv()).await {
-//             Err(_e) => (),
-//             other => panic!("Should have timed out, but got: {:?}", other),
-//         };
-//     }
-//
-//     fn add_event_response(
-//         mock: &SuiMockClient,
-//         module: Identifier,
-//         cursor: EventID,
-//         events: EventPage,
-//     ) {
-//         mock.add_event_response(BRIDGE_PACKAGE_ID, module.clone(), cursor, events.clone());
-//     }
-// }
