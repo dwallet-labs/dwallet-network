7739598d6c774895c13d7161401496abb98051e6
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-04-17 17:35:05 +0300

    wip

--- sdk/typescript/test/e2e/dwallet-mpc.test.ts
+++ sdk/typescript/test/e2e/dwallet-mpc.test.ts
@@ -40,11 +40,11 @@ describe('Test dWallet MPC', () => {
 		);
 		const address = keypair.getPublicKey().toSuiAddress();
 		console.log(`Address: ${address}`);
-		const suiClient = new SuiClient({ url: getFullnodeUrl('localnet') });
-		// const suiClient = new SuiClient({ url: 'https://fullnode.sui.beta.devnet.ika-network.net' });
+		// const suiClient = new SuiClient({ url: getFullnodeUrl('localnet') });
+		const suiClient = new SuiClient({ url: 'https://fullnode.sui.beta.devnet.ika-network.net' });
 		await requestSuiFromFaucetV1({
-			host: getFaucetHost('localnet'),
-			// host: 'https://faucet.sui.beta.devnet.ika-network.net',
+			// host: getFaucetHost('localnet'),
+			host: 'https://faucet.sui.beta.devnet.ika-network.net',
 			recipient: address,
 		});
 
