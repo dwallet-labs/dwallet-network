f0d6aceb85719ea29b3d0752140b667858eb8e87
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-04-21 15:34:13 +0300

    cr

--- crates/ika-core/src/dwallet_mpc/mod.rs
+++ crates/ika-core/src/dwallet_mpc/mod.rs
@@ -152,7 +152,7 @@ pub(crate) fn session_info_from_event(
             let deserialized_event: DWalletMPCSuiEvent<DWalletDecryptionKeyReshareRequestEvent> =
                 deserialize_event_or_dynamic_field(&event.contents)?;
             Ok(Some(
-                reshare::network_decryption_key_reshare_secp256k1_session_info(deserialized_event),
+                reshare::network_decryption_key_reshare_session_info_from_event(deserialized_event),
             ))
         }
         t if t
