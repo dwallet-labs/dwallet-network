b6634e17425047aa49761984fd28e46201d6adf4
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-04-29 17:20:50 +0300

    Wip

--- crates/ika-swarm-config/src/validator_initialization_config.rs
+++ crates/ika-swarm-config/src/validator_initialization_config.rs
@@ -20,7 +20,7 @@ use sui_types::base_types::SuiAddress;
 use sui_types::crypto::{PublicKey, SuiKeyPair};
 use sui_types::multiaddr::Multiaddr;
 
-pub const DEFAULT_NUMBER_OF_AUTHORITIES: usize = 12;
+pub const DEFAULT_NUMBER_OF_AUTHORITIES: usize = 4;
 
 // All information needed to build a NodeConfig for a validator.
 #[derive(Debug, Serialize, Deserialize)]
