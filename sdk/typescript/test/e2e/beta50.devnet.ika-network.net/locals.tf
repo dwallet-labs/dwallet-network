0d0e8d15c322bf9c03d22f936e5939cc8f79724a
Itay <itaylevy134@gmail.com>
2025-04-30 11:44:41 +0300

    creating config map

---
+++ sdk/typescript/test/e2e/beta50.devnet.ika-network.net/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  ika_chain_config = {
+    sui_chain_identifier  = "custom"
+    ika_package_id        = "0xdc1b92043f9467a550f9966adc6b86033f34a50ff55e855922e0539396ebc56f"
+    ika_system_package_id = "0x6261b56ff6785e4100049beea9e1d20cb44c43b42f6aaa8c76c608d7949f61a5"
+    ika_system_object_id  = "0xd82cdbbbfbeb579fd43af79896b63f14f494b2d9573d99583f3af1305c6dd1ef"
+  }
+}
--- sdk/typescript/test/e2e/chaos.test.ts
+++ sdk/typescript/test/e2e/chaos.test.ts
@@ -1,18 +1,33 @@
-import {Config, delay, getNetworkDecryptionKeyPublicOutput} from "../../src/dwallet-mpc/globals";
+import fs from 'fs';
+import { CoreV1Api, KubeConfig, V1ConfigMap } from '@kubernetes/client-node';
 import { beforeEach, describe, it } from 'vitest';
-import {KubeConfig, CoreV1Api} from '@kubernetes/client-node';
+
+import { Config, delay, getNetworkDecryptionKeyPublicOutput } from '../../src/dwallet-mpc/globals';
+
+const namespace = 'ika-chaos-test';
 
 const createConfigMap = async () => {
-    const kc = new KubeConfig();
-    kc.loadFromDefault();
-    const k8sApi = kc.makeApiClient(CoreV1Api);
-    const clientIdentifier = 'my-subdomain';
-    await k8sApi.createNamespacedConfigMap();
-}
+	const kc = new KubeConfig();
+	kc.loadFromDefault();
+	const k8sApi = kc.makeApiClient(CoreV1Api);
+	const clientIdentifier = 'my-subdomain';
+	const yourYamlString = fs.readFileSync(
+		'./beta50.devnet.ika-network.net/publisher/fullnode.yaml',
+		'utf8',
+	);
+	const configMap: V1ConfigMap = {
+		metadata: {
+			namespace,
+			name: 'ika-chaos-test-config',
+		},
+		data: {
+			'fullnode.yaml': yourYamlString,
+		},
+	};
+};
 
 describe('run chain chaos testing', () => {
-    it('deploy the network and run an e2e test', async () => {
-        const networkDecryptionKeyPublicOutput = await getNetworkDecryptionKeyPublicOutput(conf);
-        console.log(`networkDecryptionKeyPublicOutput: ${networkDecryptionKeyPublicOutput}`);
-    });
-};
+	it('create and deploy the config map', async () => {
+		await createConfigMap();
+	});
+});
