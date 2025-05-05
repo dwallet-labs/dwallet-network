4d00cfccf7c021b02924d7ac39093a9f7d0be236
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-05-05 16:22:45 +0300

    wip

--- nre/ika-genesis/create-ika-genesis-mac.sh
+++ nre/ika-genesis/create-ika-genesis-mac.sh
@@ -31,7 +31,7 @@ export VALIDATOR_NUM=50
 export VALIDATOR_STAKED_TOKENS_NUM=40000000000000000
 # The subdomain for Ika the network.
 #export SUBDOMAIN="localhost"
-export SUBDOMAIN="beta-with-logs.devnet.ika-network.net"
+export SUBDOMAIN="beta.devnet.ika-network.net"
 # The binary name to use.
 export BINARY_NAME="ika"
 # The directory to store the key pairs.
