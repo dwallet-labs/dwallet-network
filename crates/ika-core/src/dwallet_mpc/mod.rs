d8227ff7bc3bb2464f40c06b6e68a1b0af077f7a
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-05-05 19:05:01 +0300

    wip

--- crates/ika-core/src/dwallet_mpc/mod.rs
+++ crates/ika-core/src/dwallet_mpc/mod.rs
@@ -617,7 +617,6 @@ pub(crate) async fn session_input_from_event(
                 ReshareSecp256k1Party::generate_public_input(
                     dwallet_mpc_manager.epoch_store()?.committee().as_ref(),
                     dwallet_mpc_manager.must_get_next_active_committee().await,
-                    protocol_public_parameters,
                     dwallet_mpc_manager.get_decryption_key_share_public_parameters(
                         &deserialized_event
                             .event_data
