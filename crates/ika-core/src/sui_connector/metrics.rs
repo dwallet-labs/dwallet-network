60ae4a0c4a51566e5eb2133cba6f83038da27275
Itay <itaylevy134@gmail.com>
2025-05-06 18:05:16 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/sui_connector/metrics.rs
+++ crates/ika-core/src/sui_connector/metrics.rs
@@ -25,6 +25,11 @@ pub struct SuiConnectorMetrics {
     pub last_synced_sui_checkpoints: IntGaugeVec,
 
     pub gas_coin_balance: IntGauge,
+    pub(crate) last_checkpoint_to_write_to_sui: IntGauge,
+    pub(crate) last_checkpoint_written_to_sui: IntGauge,
+    pub(crate) new_checkpoint_to_write_to_sui_count: IntGauge,
+    pub(crate) successful_checkpoints_written_to_sui_count: IntGauge,
+    pub(crate) failed_checkpoints_written_to_sui_count: IntGauge,
 }
 
 impl SuiConnectorMetrics {
@@ -43,6 +48,36 @@ impl SuiConnectorMetrics {
                 registry,
             )
             .unwrap(),
+            last_checkpoint_to_write_to_sui: register_int_gauge_with_registry!(
+                "last_checkpoint_to_write_to_sui",
+                "The last checkpoint to write to Sui",
+                registry
+            )
+                .unwrap(),
+            last_checkpoint_written_to_sui: register_int_gauge_with_registry!(
+                "last_checkpoint_written_to_sui",
+                "The last checkpoint written to Sui",
+                registry
+            )
+                .unwrap(),
+            new_checkpoint_to_write_to_sui_count: register_int_gauge_with_registry!(
+                "new_checkpoint_to_write_to_sui_count",
+                "The number of new checkpoints to write to Sui",
+                registry
+            )
+                .unwrap(),
+            successful_checkpoints_written_to_sui_count: register_int_gauge_with_registry!(
+                "successful_checkpoints_written_to_sui_count",
+                "The number of successful checkpoints written to Sui",
+                registry
+            )
+                .unwrap(),
+            failed_checkpoints_written_to_sui_count: register_int_gauge_with_registry!(
+                "failed_checkpoints_written_to_sui_count",
+                "The number of failed checkpoints written to Sui",
+                registry
+            )
+                .unwrap(),
         };
         Arc::new(this)
     }
