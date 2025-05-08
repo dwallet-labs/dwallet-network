a19d5c8b670131f255d34c8f0d3943a709f9909d
Itay <itaylevy134@gmail.com>
2025-05-08 19:40:55 +0300

    fix

--- sdk/typescript/test/chaos-test/create-ika-genesis-mac.sh
+++ sdk/typescript/test/chaos-test/create-ika-genesis-mac.sh
@@ -107,8 +107,8 @@ done
 
 RUST_MIN_STACK=16777216
 
-RUST_MIN_STACK=$RUST_MIN_STACK cargo build --release --bin "$BINARY_NAME"
-cp ../../target/release/"$BINARY_NAME" .
+RUST_MIN_STACK=$RUST_MIN_STACK cargo build --release --bin "$BINARY_NAME" --target-dir .
+cp ./target/release/"$BINARY_NAME" .
 BINARY_NAME="$(pwd)/$BINARY_NAME"
 
 VALIDATORS_ARRAY=()
