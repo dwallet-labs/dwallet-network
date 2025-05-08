6a8ca99d0eb71310424894461c9a338867636c97
Itay <itaylevy134@gmail.com>
2025-05-08 19:11:14 +0300

    --wip-- [skip ci]

--- nre/ika-genesis/ika.default.svc.cluster.local/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  ika_chain_config = {
-    sui_chain_identifier  = "custom"
-    ika_package_id        = "0x43ba0ecb8088565fcd81ce6dbadc67b7ced96af195b085477fd05586d3c232d7"
-    ika_system_package_id = "0xdd544b6719690906d5d96913c61f7cf9f5850d25121186dd185f3348c97e433d"
-    ika_system_object_id  = "0xd415b4f6cfa95ae766ddb060bca4c527d37760b13907983a9e732b55e337bf70"
-  }
-}
