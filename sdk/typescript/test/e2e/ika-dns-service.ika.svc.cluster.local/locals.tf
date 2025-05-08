db3eb84af6362b824646023892324d1d558ef7b5
Itay <itaylevy134@gmail.com>
2025-05-08 14:26:35 +0300

    --wip-- [skip ci]

---
+++ sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  ika_chain_config = {
+    sui_chain_identifier  = "custom"
+    ika_package_id        = "0x3f7226bca6095df65533a2e26cf393b1cf256e6a7f8da9a78116ccf055c631ed"
+    ika_system_package_id = "0xa656b4ec1039dee4ee21f55e3a147ab5a4741a5d4fbf4cebe94a335e29fe87ed"
+    ika_system_object_id  = "0x67c0c046b80b3b63d5804ba034f671d0ceeba84a04e5f8d88cb048ef1bf2c2e7"
+  }
+}
