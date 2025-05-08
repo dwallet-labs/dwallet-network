724cc981c7559484e6626a5d4a0a35361c68403c
Itay <itaylevy134@gmail.com>
2025-05-08 16:30:36 +0300

    good

--- sdk/typescript/test/chaos-test/chaos.test.ts
+++ sdk/typescript/test/chaos-test/chaos.test.ts
@@ -1,217 +1,210 @@
-import {
-  CoreV1Api,
-  KubeConfig,
-  V1ConfigMap,
-  V1Namespace,
-  V1Pod
-} from "@kubernetes/client-node";
-import fs from "fs";
-import { describe, it } from "vitest";
+import fs from 'fs';
+import path from 'path';
+import { CoreV1Api, KubeConfig, V1ConfigMap, V1Namespace, V1Pod } from '@kubernetes/client-node';
+import { describe, it } from 'vitest';
+
+const CONFIG_MAP_NAME = 'ika-chaos-test-config';
+const NETWORK_SERVICE_NAME = 'ika-dns-service';
+const NAMESPACE_NAME = 'ika';
 
 const createNamespace = async (kc: KubeConfig, namespaceName: string) => {
-  const k8sApi = kc.makeApiClient(CoreV1Api);
-  const namespaceBody: V1Namespace = {
-    metadata: {
-      name: namespaceName
-    }
-  };
-  await k8sApi.createNamespace({ body: namespaceBody }).catch((err) => {
-    if (err.response?.statusCode !== 409) throw err;
-  });
+	const k8sApi = kc.makeApiClient(CoreV1Api);
+	const namespaceBody: V1Namespace = {
+		metadata: {
+			name: namespaceName,
+		},
+	};
+	await k8sApi.createNamespace({ body: namespaceBody });
 };
 
-const CONFIG_MAP_NAME = "ika-chaos-test-config";
-
 async function createConfigMap(
-  kc: KubeConfig,
-  namespaceName: string,
-  numOfValidators: number
+	kc: KubeConfig,
+	namespaceName: string,
+	numOfValidators: number,
 ): Promise<V1ConfigMap> {
-  const k8sApi = kc.makeApiClient(CoreV1Api);
-  const fullNodeYaml = fs.readFileSync(
-    "/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/publisher/fullnode.yaml",
-    "utf8"
-  );
-  const validatorsConfig: Record<string, string> = {};
+	const k8sApi = kc.makeApiClient(CoreV1Api);
+	const fullNodeYaml = fs.readFileSync(
+		path.resolve(
+			process.cwd(),
+			`${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/publisher/fullnode.yaml`,
+		),
+		'utf8',
+	);
+	const validatorsConfig: Record<string, string> = {};
 
-  for (let i = 0; i < numOfValidators; i++) {
-    validatorsConfig[`validator${i + 1}_class-groups.key`] = fs.readFileSync(
-      `/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/val${i + 1}.ika-dns-service.ika.svc.cluster.local/key-pairs/class-groups.key`,
-      "utf8"
-    );
-    validatorsConfig[`validator${i + 1}_consensus.key`] = fs.readFileSync(
-      `/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/val${i + 1}.ika-dns-service.ika.svc.cluster.local/key-pairs/consensus.key`,
-      "utf8"
-    );
-    validatorsConfig[`validator${i + 1}_network.key`] = fs.readFileSync(
-      `/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/val${i + 1}.ika-dns-service.ika.svc.cluster.local/key-pairs/network.key`,
-      "utf8"
-    );
-    validatorsConfig[`validator${i + 1}_protocol.key`] = fs.readFileSync(
-      `/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/val${i + 1}.ika-dns-service.ika.svc.cluster.local/key-pairs/protocol.key`,
-      "utf8"
-    );
-    validatorsConfig[`validator${i + 1}.yaml`] = fs.readFileSync(
-      `/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/val${i + 1}.ika-dns-service.ika.svc.cluster.local/validator.yaml`,
-      "utf-8"
-    );
-  }
+	for (let i = 0; i < numOfValidators; i++) {
+		validatorsConfig[`validator${i + 1}_class-groups.key`] = fs.readFileSync(
+			`/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/val${i + 1}.${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/key-pairs/class-groups.key`,
+			'utf8',
+		);
+		validatorsConfig[`validator${i + 1}_consensus.key`] = fs.readFileSync(
+			`/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/val${i + 1}.${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/key-pairs/consensus.key`,
+			'utf8',
+		);
+		validatorsConfig[`validator${i + 1}_network.key`] = fs.readFileSync(
+			`/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/val${i + 1}.${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/key-pairs/network.key`,
+			'utf8',
+		);
+		validatorsConfig[`validator${i + 1}_protocol.key`] = fs.readFileSync(
+			`/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/val${i + 1}.${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/key-pairs/protocol.key`,
+			'utf8',
+		);
+		validatorsConfig[`validator${i + 1}.yaml`] = fs.readFileSync(
+			`/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/val${i + 1}.${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/validator.yaml`,
+			'utf-8',
+		);
+	}
 
-  const configMap: V1ConfigMap = {
-    metadata: {
-      namespace: namespaceName,
-      name: CONFIG_MAP_NAME
-    },
-    data: {
-      "fullnode.yaml": fullNodeYaml,
-      "notifier.key": fs.readFileSync(
-        "/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/ika-dns-service.ika.svc.cluster.local/publisher/sui_config/publisher.key",
-        "utf8"
-      ),
-      ...validatorsConfig
-    }
-  };
-  return await k8sApi.createNamespacedConfigMap({
-    namespace: namespaceName,
-    body: configMap
-  });
+	const configMap: V1ConfigMap = {
+		metadata: {
+			namespace: namespaceName,
+			name: CONFIG_MAP_NAME,
+		},
+		data: {
+			'fullnode.yaml': fullNodeYaml,
+			'notifier.key': fs.readFileSync(
+				`/Users/itaylevy/code/dwallet-network/sdk/typescript/test/e2e/${NETWORK_SERVICE_NAME}.${NAMESPACE_NAME}.svc.cluster.local/publisher/sui_config/publisher.key`,
+				'utf8',
+			),
+			...validatorsConfig,
+		},
+	};
+	return await k8sApi.createNamespacedConfigMap({
+		namespace: namespaceName,
+		body: configMap,
+	});
 }
 
-async function createNetworkServices(
-  kc: KubeConfig,
-  namespaceName: string,
-) {
-  const k8sApi = kc.makeApiClient(CoreV1Api);
-  await k8sApi.createNamespacedService({
-    namespace: namespaceName,
-    body: {
-      metadata: {
-        name: "ika-dns-service"
-      },
-      spec: {
-        clusterIP: "None",
-        ports: [
-          {
-            name: "tx-interface", 
-            protocol: "TCP",
-            port: 8080,
-            targetPort: 8080
-          },
-          {
-            name: "p2p-sync",
-            protocol: "UDP", 
-            port: 8084,
-            targetPort: 8084
-          },
-          {
-            name: "metrics",
-            protocol: "TCP",
-            port: 9184,
-            targetPort: 9184
-          },
-          {
-            name: "admin",
-            protocol: "TCP",
-            port: 1337,
-            targetPort: 1337
-          }
-        ],
-        selector: {
-          "app": "validator"
-        },
-        sessionAffinity: "None",
-        ipFamilies: ["IPv4"],
-        ipFamilyPolicy: "SingleStack",
-        internalTrafficPolicy: "Cluster"
-      }
-    }
-  });
+async function createNetworkServices(kc: KubeConfig, namespaceName: string) {
+	const k8sApi = kc.makeApiClient(CoreV1Api);
+	await k8sApi.createNamespacedService({
+		namespace: namespaceName,
+		body: {
+			metadata: {
+				name: 'ika-dns-service',
+			},
+			spec: {
+				clusterIP: 'None',
+				ports: [
+					{
+						name: 'tx-interface',
+						protocol: 'TCP',
+						port: 8080,
+						targetPort: 8080,
+					},
+					{
+						name: 'p2p-sync',
+						protocol: 'UDP',
+						port: 8084,
+						targetPort: 8084,
+					},
+					{
+						name: 'metrics',
+						protocol: 'TCP',
+						port: 9184,
+						targetPort: 9184,
+					},
+					{
+						name: 'admin',
+						protocol: 'TCP',
+						port: 1337,
+						targetPort: 1337,
+					},
+				],
+				selector: {
+					app: 'validator',
+				},
+				sessionAffinity: 'None',
+				ipFamilies: ['IPv4'],
+				ipFamilyPolicy: 'SingleStack',
+				internalTrafficPolicy: 'Cluster',
+			},
+		},
+	});
 }
 
 async function createPods(kc: KubeConfig, namespaceName: string, numOfValidators: number) {
-  const k8sApi = kc.makeApiClient(CoreV1Api);
-  for (let i = 0; i < numOfValidators; i++) {
-    const pod: V1Pod = {
-      metadata: {
-        name: `ika-val-${i + 1}`,
-        namespace: namespaceName,
-        labels: {
-          "app": "validator"
-        }
-      },
-      spec: {
-        hostname: `val${i + 1}`,
-        subdomain: "ika-dns-service",
-        containers: [
-          {
-            env: [
-              {
-                name: "RUST_LOG",
-                value: "off,ika_node=info,ika_core=debug"
-              },
-              {
-                name: "RUST_MIN_STACK",
-                value: "16777216"
-              }
-            ],
-            command: ["/opt/ika/bin/ika-node", "--config-path", "/opt/ika/config/validator.yaml"],
-            name: "ika-node",
-            image: "ika:devnet-v0.0.6-arm64",
-          }
-        ],
-        restartPolicy: "Always"
-      }
-    };
-    await k8sApi.createNamespacedPod({
-      namespace: namespaceName,
-      body: pod
-    });
-  }
-  let fullnodePod = {
-    metadata: {
-      name: `ika-fullnode`,
-      namespace: namespaceName
-    },
-    spec: {
-      hostname: 'fullnode',
-      subdomain: "ika-dns-service",
-      containers: [
-        {
-          env: [
-            {
-              name: "RUST_LOG",
-              value: "off,ika_node=info,ika_core=debug"
-            },
-            {
-              name: "RUST_MIN_STACK",
-              value: "16777216"
-            }
-          ],
-          command: ["/opt/ika/bin/ika-node", "--config-path", "/opt/ika/config/fullnode.yaml"],
-          name: "ika-node",
-          image: "ika:devnet-v0.0.6-arm64",
-        }
-      ],
-      restartPolicy: "Always"
-    }
-  };
-  await k8sApi.createNamespacedPod({
-    namespace: namespaceName,
-    body: fullnodePod
-  });
+	const k8sApi = kc.makeApiClient(CoreV1Api);
+	for (let i = 0; i < numOfValidators; i++) {
+		const pod: V1Pod = {
+			metadata: {
+				name: `ika-val-${i + 1}`,
+				namespace: namespaceName,
+				labels: {
+					app: 'validator',
+				},
+			},
+			spec: {
+				hostname: `val${i + 1}`,
+				subdomain: 'ika-dns-service',
+				containers: [
+					{
+						env: [
+							{
+								name: 'RUST_LOG',
+								value: 'off,ika_node=info,ika_core=debug',
+							},
+							{
+								name: 'RUST_MIN_STACK',
+								value: '16777216',
+							},
+						],
+						command: ['/opt/ika/bin/ika-node', '--config-path', '/opt/ika/config/validator.yaml'],
+						name: 'ika-node',
+						image: 'ika:devnet-v0.0.6-arm64',
+					},
+				],
+				restartPolicy: 'Always',
+			},
+		};
+		await k8sApi.createNamespacedPod({
+			namespace: namespaceName,
+			body: pod,
+		});
+	}
+	const fullnodePod = {
+		metadata: {
+			name: `ika-fullnode`,
+			namespace: namespaceName,
+		},
+		spec: {
+			hostname: 'fullnode',
+			subdomain: 'ika-dns-service',
+			containers: [
+				{
+					env: [
+						{
+							name: 'RUST_LOG',
+							value: 'off,ika_node=info,ika_core=debug',
+						},
+						{
+							name: 'RUST_MIN_STACK',
+							value: '16777216',
+						},
+					],
+					command: ['/opt/ika/bin/ika-node', '--config-path', '/opt/ika/config/fullnode.yaml'],
+					name: 'ika-node',
+					image: 'ika:devnet-v0.0.6-arm64',
+				},
+			],
+			restartPolicy: 'Always',
+		},
+	};
+	await k8sApi.createNamespacedPod({
+		namespace: namespaceName,
+		body: fullnodePod,
+	});
 }
 
-describe("run chain chaos testing", () => {
-  it("create and deploy the config map", async () => {
-    const kc = new KubeConfig();
-    kc.loadFromDefault();
-    const namespaceName = "ika";
-    console.log(`Creating namespace: ${namespaceName}`);
-    await createNamespace(kc, namespaceName);
-    const configMap = await createConfigMap(kc, namespaceName, 4);
-    console.log(`ConfigMap created: ${configMap}`);
-    await createPods(kc, namespaceName, 4);
-    await createNetworkServices(kc, namespaceName);
-  });
+describe('run chain chaos testing', () => {
+	it('create and deploy the config map', async () => {
+		const kc = new KubeConfig();
+		kc.loadFromDefault();
+		console.log(`Creating namespace: ${NAMESPACE_NAME}`);
+		await createNamespace(kc, NAMESPACE_NAME);
+		const configMap = await createConfigMap(kc, NAMESPACE_NAME, 4);
+		console.log(`ConfigMap created: ${configMap}`);
+		await createPods(kc, NAMESPACE_NAME, 4);
+		await createNetworkServices(kc, NAMESPACE_NAME);
+	});
 });
-
