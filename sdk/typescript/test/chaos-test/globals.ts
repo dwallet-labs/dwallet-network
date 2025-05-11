fba6486da1bbd7d4f725c6a73eff1025a5575a88
Itay <itaylevy134@gmail.com>
2025-05-11 13:22:50 +0300

    good

--- sdk/typescript/test/chaos-test/globals.ts
+++ sdk/typescript/test/chaos-test/globals.ts
@@ -2,4 +2,3 @@ export const CONFIG_MAP_NAME = 'ika-chaos-test-config';
 export const NETWORK_SERVICE_NAME = 'ika-dns-service';
 export const NAMESPACE_NAME = 'ika';
 export const TEST_ROOT_DIR = `${process.cwd()}/test/chaos-test`;
-export const TEST_ROOT_DIR_FUN = () => `${process.cwd()}/test/chaos-test`;
