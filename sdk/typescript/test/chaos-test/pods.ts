6c41cb656a5fcbaee2ec37a66c7c3c0a77a9e1ee
Itay <itaylevy134@gmail.com>
2025-05-11 13:06:09 +0300

    volumes back

--- sdk/typescript/test/chaos-test/pods.ts
+++ sdk/typescript/test/chaos-test/pods.ts
@@ -1,6 +1,7 @@
 import type { KubeConfig, V1Pod } from '@kubernetes/client-node';
 import { CoreV1Api } from '@kubernetes/client-node';
-import { NETWORK_SERVICE_NAME } from './globals';
+
+import { CONFIG_MAP_NAME, NETWORK_SERVICE_NAME } from './globals';
 
 export async function createPods(kc: KubeConfig, namespaceName: string, numOfValidators: number) {
 	const k8sApi = kc.makeApiClient(CoreV1Api);
@@ -31,6 +32,63 @@ export async function createPods(kc: KubeConfig, namespaceName: string, numOfVal
 						command: ['/opt/ika/bin/ika-node', '--config-path', '/opt/ika/config/validator.yaml'],
 						name: 'ika-node',
 						image: process.env.DOCKER_TAG,
+						volumeMounts: [
+							{
+								name: 'config-vol',
+								mountPath: '/opt/ika/key-pairs/class-groups.key',
+								subPath: 'class-groups.key',
+							},
+							{
+								name: 'config-vol',
+								mountPath: '/opt/ika/key-pairs/consensus.key',
+								subPath: 'consensus.key',
+							},
+							{
+								name: 'config-vol',
+								mountPath: '/opt/ika/key-pairs/network.key',
+								subPath: 'network.key',
+							},
+							{
+								name: 'config-vol',
+								mountPath: '/opt/ika/key-pairs/protocol.key',
+								subPath: 'protocol.key',
+							},
+							{
+								name: 'config-vol',
+								mountPath: '/opt/ika/config/validator.yaml',
+								subPath: 'validator.yaml',
+							},
+						],
+					},
+				],
+				volumes: [
+					{
+						name: 'config-vol',
+						configMap: {
+							name: CONFIG_MAP_NAME,
+							items: [
+								{
+									key: `validator${i + 1}_class-groups.key`,
+									path: 'class-groups.key',
+								},
+								{
+									key: `validator${i + 1}_consensus.key`,
+									path: 'consensus.key',
+								},
+								{
+									key: `validator${i + 1}_network.key`,
+									path: 'network.key',
+								},
+								{
+									key: `validator${i + 1}_protocol.key`,
+									path: 'protocol.key',
+								},
+								{
+									key: `validator${i + 1}.yaml`,
+									path: 'validator.yaml',
+								},
+							],
+						},
 					},
 				],
 				restartPolicy: 'Always',
@@ -64,6 +122,36 @@ export async function createPods(kc: KubeConfig, namespaceName: string, numOfVal
 					command: ['/opt/ika/bin/ika-node', '--config-path', '/opt/ika/config/fullnode.yaml'],
 					name: 'ika-node',
 					image: process.env.DOCKER_TAG,
+					volumeMounts: [
+						{
+							name: 'config-vol',
+							mountPath: '/opt/ika/key-pairs/notifier.key',
+							subPath: 'notifier.key',
+						},
+						{
+							name: 'config-vol',
+							mountPath: '/opt/ika/config/fullnode.yaml',
+							subPath: 'fullnode.yaml',
+						},
+					],
+				},
+			],
+			volumes: [
+				{
+					name: 'config-vol',
+					configMap: {
+						name: CONFIG_MAP_NAME,
+						items: [
+							{
+								key: `notifier.key`,
+								path: 'notifier.key',
+							},
+							{
+								key: `fullnode.yaml`,
+								path: 'fullnode.yaml',
+							},
+						],
+					},
 				},
 			],
 			restartPolicy: 'Always',
