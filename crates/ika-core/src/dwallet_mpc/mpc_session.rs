ff7b9190c0fa1221f0deda5982d6577e4f662e47
Itay <itaylevy134@gmail.com>
2025-05-11 17:42:20 +0300

    fix

--- crates/ika-core/src/dwallet_mpc/mpc_session.rs
+++ crates/ika-core/src/dwallet_mpc/mpc_session.rs
@@ -88,6 +88,7 @@ pub(super) struct DWalletMPCSession {
     weighted_threshold_access_structure: WeightedThresholdAccessStructure,
     pub(crate) mpc_event_data: Option<MPCEventData>,
     pub(crate) received_more_messages_since_last_retry: bool,
+    pub(crate) attempts_count: usize,
 }
 
 impl DWalletMPCSession {
@@ -314,6 +315,7 @@ impl DWalletMPCSession {
             self.new_dwallet_report_threshold_not_reached(ThresholdNotReachedReport {
                 session_id: self.session_id,
                 crypto_round_number: self.pending_quorum_for_highest_round_number,
+                attempt: self.attempts_count
             })?;
         let epoch_store = self.epoch_store()?.clone();
         let consensus_adapter = self.consensus_adapter.clone();
--- crates/ika-types/src/messages_dwallet_mpc.rs
+++ crates/ika-types/src/messages_dwallet_mpc.rs
@@ -353,6 +353,7 @@ pub struct MaliciousReport {
 pub struct ThresholdNotReachedReport {
     pub session_id: ObjectID,
     pub crypto_round_number: usize,
+    pub attempt: usize
 }
 
 impl MaliciousReport {
