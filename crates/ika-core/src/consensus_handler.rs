a474ebc74bd16a218e58da0b69de62ffed67504f
Itay <itaylevy134@gmail.com>
2025-05-12 10:35:51 +0300

    fmt

--- crates/ika-core/src/consensus_handler.rs
+++ crates/ika-core/src/consensus_handler.rs
@@ -532,9 +532,7 @@ pub(crate) fn classify(transaction: &ConsensusTransaction) -> &'static str {
         ConsensusTransactionKind::DWalletMPCMessage(..) => "dwallet_mpc_message",
         ConsensusTransactionKind::DWalletMPCOutput(..) => "dwallet_mpc_output",
         ConsensusTransactionKind::CapabilityNotificationV1(_) => "capability_notification_v1",
-        ConsensusTransactionKind::DWalletMPCMaliciousReport(..) => {
-            "dwallet_mpc_malicious_report"
-        }
+        ConsensusTransactionKind::DWalletMPCMaliciousReport(..) => "dwallet_mpc_malicious_report",
         ConsensusTransactionKind::DWalletMPCThresholdNotReached(..) => {
             "dwallet_mpc_threshold_not_reached"
         }
