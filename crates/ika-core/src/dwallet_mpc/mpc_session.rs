6c1081227f41876dd7e6401c8b48f93cf1d90196
Itay <itaylevy134@gmail.com>
2025-05-12 16:48:15 +0300

    good

--- crates/ika-core/src/dwallet_mpc/mpc_session.rs
+++ crates/ika-core/src/dwallet_mpc/mpc_session.rs
@@ -295,8 +295,7 @@ impl DWalletMPCSession {
         mut malicious_parties_ids: Vec<PartyID>,
     ) -> DwalletMPCResult<()> {
         // Makes sure all the validators report on the malicious actors in the same order without duplicates.
-        malicious_parties_ids.sort();
-        malicious_parties_ids.dedup();
+        malicious_parties_ids.deduplicate_and_sort();
         let report = MaliciousReport::new(
             party_ids_to_authority_names(&malicious_parties_ids, &*self.epoch_store()?)?,
             self.session_id.clone(),
