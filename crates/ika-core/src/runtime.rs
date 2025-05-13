18c03531a0768630d03debea6f8757f583db3cfc
Itay <itaylevy134@gmail.com>
2025-05-13 17:37:38 +0300

    chill

--- crates/ika-core/src/runtime.rs
+++ crates/ika-core/src/runtime.rs
@@ -3,6 +3,7 @@
 
 use ika_config::NodeConfig;
 use tokio::runtime::Runtime;
+use tracing::error;
 
 pub struct IkaRuntimes {
     // Order in this struct is the order in which runtimes are stopped
@@ -12,8 +13,16 @@ pub struct IkaRuntimes {
 
 impl IkaRuntimes {
     pub fn new(_confg: &NodeConfig) -> Self {
+        if let Err(err) = rayon::ThreadPoolBuilder::new()
+            .panic_handler(|err| error!("Rayon thread pool task panicked: {:?}", err))
+            .stack_size(16 * 1024 * 1024)
+            .build_global()
+        {
+            error!("Failed to create rayon thread pool: {:?}", err);
+        }
         let ika_node = tokio::runtime::Builder::new_multi_thread()
             .thread_name("ika-node-runtime")
+            .thread_stack_size(16 * 1024 * 1024)
             .enable_all()
             .build()
             .unwrap();
