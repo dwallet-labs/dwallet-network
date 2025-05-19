0721b5b1411e78d6399a1856382d3356dd56dae2
Itay <itaylevy134@gmail.com>
2025-05-19 15:10:10 +0300

    replaced verification with scaly code

--- crates/ika-core/src/dwallet_mpc/encrypt_user_share.rs
+++ crates/ika-core/src/dwallet_mpc/encrypt_user_share.rs
@@ -1,22 +1,14 @@
-use class_groups::{
-    CiphertextSpaceGroupElement, CiphertextSpaceValue, SECP256K1_FUNDAMENTAL_DISCRIMINANT_LIMBS,
-    SECP256K1_NON_FUNDAMENTAL_DISCRIMINANT_LIMBS,
-};
 use dwallet_mpc_types::dwallet_mpc::{
     MPCPublicOutput, MPCPublicOutputClassGroups, SerializedWrappedMPCPublicOutput,
 };
-use fastcrypto::traits::{ToFromBytes, VerifyingKey};
-use group::GroupElement;
-use homomorphic_encryption::GroupsPublicParametersAccessors;
+use fastcrypto::traits::ToFromBytes;
 use ika_types::dwallet_mpc_error::{DwalletMPCError, DwalletMPCResult};
 use ika_types::messages_dwallet_mpc::EncryptedShareVerificationRequestEvent;
-use std::marker::PhantomData;
-use twopc_mpc::languages::class_groups::construct_encryption_of_discrete_log_public_parameters;
-use twopc_mpc::secp256k1;
+use rand_core::OsRng;
+use twopc_mpc::dkg::Protocol;
 use twopc_mpc::secp256k1::class_groups::{
-    AsyncProtocol, EncryptionOfSecretShareProof, ProtocolPublicParameters,
+    AsyncProtocol,
 };
-use twopc_mpc::secp256k1::SCALAR_LIMBS;
 
 /// Verifies that the given encrypted secret key share matches the encryption of the dWallet's
 /// secret share, validates the signature on the dWallet's public share,
@@ -45,44 +37,9 @@ fn verify_centralized_secret_key_share_proof(
     let dkg_public_output = bcs::from_bytes(serialized_dkg_public_output)?;
     match dkg_public_output {
         MPCPublicOutput::ClassGroups(MPCPublicOutputClassGroups::V1(dkg_public_output)) => {
-            let decentralized_public_output: <AsyncProtocol as twopc_mpc::dkg::Protocol>::DecentralizedPartyDKGOutput =
-                bcs::from_bytes(&dkg_public_output)?;
-            let protocol_public_params: ProtocolPublicParameters =
-                bcs::from_bytes(protocol_public_parameters)?;
-            let language_public_parameters = construct_encryption_of_discrete_log_public_parameters::<
-                SCALAR_LIMBS,
-                { SECP256K1_FUNDAMENTAL_DISCRIMINANT_LIMBS },
-                { SECP256K1_NON_FUNDAMENTAL_DISCRIMINANT_LIMBS },
-                secp256k1::GroupElement,
-            >(
-                protocol_public_params.scalar_group_public_parameters,
-                protocol_public_params.group_public_parameters.clone(),
-                bcs::from_bytes(encryption_key)?,
-            );
-            let (proof, encrypted_centralized_secret_key_share): (
-                EncryptionOfSecretShareProof,
-                CiphertextSpaceValue<SECP256K1_NON_FUNDAMENTAL_DISCRIMINANT_LIMBS>,
-            ) = bcs::from_bytes(encrypted_centralized_secret_share_and_proof)?;
-            let encrypted_centralized_secret_key_share_for_statement =
-                CiphertextSpaceGroupElement::new(
-                    encrypted_centralized_secret_key_share,
-                    &language_public_parameters
-                        .encryption_scheme_public_parameters
-                        .ciphertext_space_public_parameters(),
-                )?;
-            let centralized_public_key_share = secp256k1::GroupElement::new(
-                decentralized_public_output.centralized_party_public_key_share,
-                &protocol_public_params.group_public_parameters,
-            )?;
-            let statement = (
-                encrypted_centralized_secret_key_share_for_statement,
-                centralized_public_key_share,
-            )
-                .into();
-
-            proof
-                .verify(&PhantomData, &language_public_parameters, vec![statement])
-                .map_err(Into::into)
+            <AsyncProtocol as twopc_mpc::dkg::Protocol>::verify_encryption_of_centralized_party_share_proof(
+                &bcs::from_bytes(&protocol_public_parameters)?, bcs::from_bytes(&dkg_public_output)?, bcs::from_bytes(&encryption_key)?, bcs::from_bytes(&encrypted_centralized_secret_share_and_proof)?, &mut OsRng).map_err(Into::into)?;
+            Ok(())
         }
     }
 }
