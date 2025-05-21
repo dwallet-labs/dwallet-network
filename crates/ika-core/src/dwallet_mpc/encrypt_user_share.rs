2d8e057ae32d3e8ed39eb366b986d1a838455a6e
Itay <itaylevy134@gmail.com>
2025-05-21 18:12:30 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/dwallet_mpc/encrypt_user_share.rs
+++ crates/ika-core/src/dwallet_mpc/encrypt_user_share.rs
@@ -38,5 +38,11 @@ fn verify_centralized_secret_key_share_proof(
     protocol_public_parameters: &Vec<u8>,
 ) -> anyhow::Result<()> {
     <AsyncProtocol as Protocol>::verify_encryption_of_centralized_party_share_proof(
-                &bcs::from_bytes(&protocol_public_parameters)?, bcs::from_bytes(&serialized_dkg_public_output)?, bcs::from_bytes(&encryption_key)?, bcs::from_bytes(&encrypted_centralized_secret_share_and_proof)?, &mut OsRng).map_err(Into::<anyhow::Error>::into)?;
+        &bcs::from_bytes(&protocol_public_parameters)?,
+        bcs::from_bytes(&serialized_dkg_public_output)?,
+        bcs::from_bytes(&encryption_key)?,
+        bcs::from_bytes(&encrypted_centralized_secret_share_and_proof)?,
+        &mut OsRng,
+    )
+    .map_err(Into::<anyhow::Error>::into)?;
 }
