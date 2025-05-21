05b6494600e126f3d2c77b6bbb186f0ad98d4460
Itay <itaylevy134@gmail.com>
2025-05-21 18:21:51 +0300

    verify_secret_share

--- crates/ika-core/src/dwallet_mpc/make_dwallet_user_secret_key_shares_public.rs
+++ crates/ika-core/src/dwallet_mpc/make_dwallet_user_secret_key_shares_public.rs
@@ -1,6 +1,6 @@
 use anyhow::anyhow;
 use class_groups::Secp256k1EncryptionKey;
-use dwallet_mpc_types::dwallet_mpc::{MPCPublicOutput, SerializedWrappedMPCPublicOutput};
+use dwallet_mpc_types::dwallet_mpc::{DWalletDKGSecondOutputVersion, ImportedSecretShareVersions, MPCPublicOutput, SerializedWrappedMPCPublicOutput};
 use group::{CyclicGroupElement, GroupElement};
 use homomorphic_encryption::{AdditivelyHomomorphicEncryptionKey, PlaintextSpaceGroupElement};
 use twopc_mpc::secp256k1::class_groups::AsyncProtocol;
@@ -13,9 +13,9 @@ pub fn verify_secret_share(
     secret_share: Vec<u8>,
     dkg_output: SerializedWrappedMPCPublicOutput,
 ) -> anyhow::Result<()> {
-    let secret_share: MPCPublicOutput = bcs::from_bytes(&secret_share)?;
+    let secret_share: ImportedSecretShareVersions = bcs::from_bytes(&secret_share)?;
     let secret_share = match secret_share {
-        MPCPublicOutput::V1(output) => output,
+        ImportedSecretShareVersions::V1(output) => output,
         _ => {
             return Err(anyhow!(
                 "invalid centralized public output version: expected ClassGroups::V1, got {:?}",
@@ -25,7 +25,7 @@ pub fn verify_secret_share(
     };
     let dkg_output = bcs::from_bytes(&dkg_output)?;
     match dkg_output {
-        MPCPublicOutput::V1(dkg_output) => {
+        DWalletDKGSecondOutputVersion::V1(dkg_output) => {
             <AsyncProtocol as twopc_mpc::dkg::Protocol>::verify_centralized_party_secret_key_share(
                 &bcs::from_bytes(protocol_public_parameters)?,
                 bcs::from_bytes(&dkg_output)?,
