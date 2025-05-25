59c7d59da69b5c3b229fb3cc71d8a6ee2c45047a
Itay <itaylevy134@gmail.com>
2025-05-25 15:29:38 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/dwallet_mpc/mpc_outputs_verifier.rs
+++ crates/ika-core/src/dwallet_mpc/mpc_outputs_verifier.rs
@@ -277,16 +277,6 @@ impl DWalletMPCOutputsVerifier {
                     .import_dwallet_verification_round_completions_count
                     .inc();
             }
-            MPCProtocolInitData::MakeDWalletUserSecretKeySharesPublicRequest(_) => {
-                self.dwallet_mpc_metrics
-                    .make_dwallet_user_secret_key_shares_public_round_completions_count
-                    .inc();
-            }
-            MPCProtocolInitData::DWalletImportedKeyVerificationRequestEvent(_) => {
-                self.dwallet_mpc_metrics
-                    .import_dwallet_verification_round_completions_count
-                    .inc();
-            }
         }
     }
 }
