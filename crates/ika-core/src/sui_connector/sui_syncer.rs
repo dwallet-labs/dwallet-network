26e5cf580e987142ef70c6b349ed9b80b0abced2
Itay <itaylevy134@gmail.com>
2025-05-27 19:23:31 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/sui_connector/sui_syncer.rs
+++ crates/ika-core/src/sui_connector/sui_syncer.rs
@@ -173,7 +173,13 @@ where
             .iter()
             .map(|validator| {
                 (
-                    validator.authority_name(),
+                    validator
+                        .validator_info
+                        .name
+                        .clone()
+                        .into_bytes()
+                        .try_into()
+                        .unwrap(),
                     validator.validator_info.protocol_pubkey.bytes.clone(),
                 )
             })
