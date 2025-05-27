721bf285af05d462fc9952f3898ab1a4711617e6
Itay <itaylevy134@gmail.com>
2025-05-27 14:17:28 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/dwallet_mpc/dwallet_mpc_metrics.rs
+++ crates/ika-core/src/dwallet_mpc/dwallet_mpc_metrics.rs
@@ -10,51 +10,55 @@ use dwallet_mpc_types::dwallet_mpc::{
     VALIDATOR_DATA_FOR_SECRET_SHARE_STRUCT_NAME,
 };
 use im::HashMap;
-use prometheus::{register_int_gauge_with_registry, IntGauge, Registry};
+use prometheus::{register_int_gauge_vec_with_registry, register_int_gauge_with_registry, IntGauge, IntGaugeVec, Registry};
 use std::sync::Arc;
 
-struct MPCProtocolRoundMetrics {
-    advance_calls: IntGauge,
-    advance_completions: IntGauge,
-
-    completions_count: IntGauge,
-    last_completion_duration: IntGauge,
-}
-
-struct MPCProtocolMetrics {
-    received_events_start_count: IntGauge,
-    rounds_metrics: HashMap<usize, MPCProtocolRoundMetrics>,
-}
-
 pub struct DWalletMPCMetrics {
-    protocols_metrics: HashMap<usize, MPCProtocolMetrics>,
+    received_events_start_count: IntGaugeVec,
+    advance_calls: IntGaugeVec,
+    advance_completions: IntGaugeVec,
+    completions_count: IntGaugeVec,
+    last_completion_duration: IntGaugeVec,
 }
 
 impl DWalletMPCMetrics {
     pub fn new(registry: &Registry) -> Arc<Self> {
-        let mut protocols_metrics = HashMap::new();
-        let mpc_protocols = vec![
-            START_NETWORK_DKG_EVENT_STRUCT_NAME,
-            DWALLET_DKG_FIRST_ROUND_REQUEST_EVENT_STRUCT_NAME,
-            DWALLET_MAKE_DWALLET_USER_SECRET_KEY_SHARES_PUBLIC_REQUEST_EVENT,
-            DWALLET_IMPORTED_KEY_VERIFICATION_REQUEST_EVENT,
-            // TODO (#650): Rename Move structs
-            DWALLET_DKG_SECOND_ROUND_REQUEST_EVENT_STRUCT_NAME,
-            // TODO (#650): Rename Move structs
-            PRESIGN_REQUEST_EVENT_STRUCT_NAME,
-            SIGN_REQUEST_EVENT_STRUCT_NAME,
-            LOCKED_NEXT_COMMITTEE_EVENT_STRUCT_NAME,
-            VALIDATOR_DATA_FOR_SECRET_SHARE_STRUCT_NAME,
-            START_NETWORK_DKG_EVENT_STRUCT_NAME,
-            ENCRYPTED_SHARE_VERIFICATION_REQUEST_EVENT_NAME,
-            FUTURE_SIGN_REQUEST_EVENT_NAME,
-            DWALLET_DECRYPTION_KEY_RESHARE_REQUEST_EVENT_NAME,
-        ];
-        for protocol in mpc_protocols {
-            
-        }
-        Self {
-            protocols_metrics
-        }
+        Arc::new(Self {
+            received_events_start_count: register_int_gauge_vec_with_registry!(,
+                "dwallet_mpc_received_events_start_count",
+                "Number of received events start",
+                &["event_name"],
+                registry
+            )
+            .unwrap(),
+            advance_calls: register_int_gauge_vec_with_registry!(
+                "dwallet_mpc_advance_calls",
+                "Number of advance calls",
+                &["event_name"],
+                registry
+            )
+            .unwrap(),
+            advance_completions: register_int_gauge_vec_with_registry!(
+                "dwallet_mpc_advance_completions",
+                "Number of advance completions",
+                &["event_name"],
+                registry
+            )
+            .unwrap(),
+            completions_count: register_int_gauge_vec_with_registry!(
+                "dwallet_mpc_completions_count",
+                "Number of completions",
+                &["event_name"],
+                registry
+            )
+            .unwrap(),
+            last_completion_duration: register_int_gauge_vec_with_registry!(
+                "dwallet_mpc_last_completion_duration",
+                "Duration of the last completion in milliseconds",
+                &["event_name"],
+                registry
+            )
+            .unwrap(),
+        })
     }
 }
