e3ad28d560eadbeac9931c56606508912cc52662
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-06-03 16:27:05 +0300

    wip

--- crates/ika-core/src/dwallet_mpc/mpc_session.rs
+++ crates/ika-core/src/dwallet_mpc/mpc_session.rs
@@ -155,6 +155,8 @@ impl DWalletMPCSession {
     /// computation, and Tokio, which is good for IO heavy tasks, is used to submit the result to
     /// the consensus.
     pub(super) fn advance(&mut self, tokio_runtime_handle: &Handle) -> DwalletMPCResult<()> {
+        // Make sure we transfer only the messages up to the current round
+        // (exclude messages that might be recieved from future rounds)
         self.serialized_full_messages
             .retain(|round, _| round < &self.current_round);
         // Safe to unwrap as advance can only be called after the event is received.
