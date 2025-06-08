715016af7b57d1373795ded14af613e874d3a79e
Itay <itaylevy134@gmail.com>
2025-06-08 16:57:04 +0300

    --wip-- [skip ci]

--- sdk/typescript/test/chaos-test/pods.ts
+++ sdk/typescript/test/chaos-test/pods.ts
@@ -133,7 +133,7 @@ export async function createPods(kc: KubeConfig, namespaceName: string, numOfVal
 					env: [
 						{
 							name: 'RUST_LOG',
-							value: 'off,ika_node=info,ika_core=warn,consensus_core=info',
+							value: 'off,ika_node=info,ika_core=warn',
 						},
 						{
 							name: 'RUST_MIN_STACK',
