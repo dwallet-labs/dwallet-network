44bb263a6524ca1ee1acac51a7e6815ab2935c11
Itay <itaylevy134@gmail.com>
2025-06-11 17:46:03 +0300

    good

--- crates/ika-core/src/dwallet_mpc/mpc_session.rs
+++ crates/ika-core/src/dwallet_mpc/mpc_session.rs
@@ -51,34 +51,14 @@ pub(crate) struct ReadyToAdvanceCheckResult {
 
 #[derive(Clone)]
 pub enum PublicInput {
-    /// Import a secret key to a dWallet.
     DWalletImportedKeyVerificationRequest(
         <DWalletImportedKeyVerificationParty as mpc::Party>::PublicInput,
     ),
-
-    /// The first round of the DKG protocol.
     DKGFirst(<DKGFirstParty as mpc::Party>::PublicInput),
-    /// The second round of the DKG protocol.
-    /// Contains the data of the event that triggered the round,
-    /// and the network key version of the first round.
     DKGSecond(<DKGSecondParty as mpc::Party>::PublicInput),
-    /// The first round of the Presign protocol for each message in the Batch.
-    /// Contains the `ObjectId` of the dWallet object,
-    /// the DKG decentralized output, the batch session ID (same for each message in the batch),
-    /// and the dWallets network key version.
     Presign(<PresignParty as mpc::Party>::PublicInput),
-    /// The first and only round of the Sign protocol.
-    /// Contains all the data needed to sign the message.
     Sign(<SignFirstParty as mpc::Party>::PublicInput),
-    /// The only round of the network DKG protocol.
-    /// Contains the network key scheme, the dWallet network decryption key object ID
-    /// and at the end of the session holds the new key version.
     NetworkEncryptionKeyDkg(<Secp256k1Party as mpc::Party>::PublicInput),
-    /// The round of verifying the encrypted share proof is valid and
-    /// that the signature on it is valid.
-    /// This is not a real MPC round,
-    /// but we use it to start the verification process using the same events mechanism
-    /// because the system does not support native functions.
     EncryptedShareVerification(twopc_mpc::secp256k1::class_groups::ProtocolPublicParameters),
     PartialSignatureVerification(twopc_mpc::secp256k1::class_groups::ProtocolPublicParameters),
     NetworkEncryptionKeyReconfiguration(<ReshareSecp256k1Party as mpc::Party>::PublicInput),
