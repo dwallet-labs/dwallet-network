d9f4f0b811825688ebd43b38af715f055d2e0b61
Itay Levy <itaylevy134@gmail.com>
2025-06-12 14:11:47 +0300

    fix: Stop wiping outputs after receiving a late session event (#1148)

--- crates/ika-core/src/consensus_handler.rs
+++ crates/ika-core/src/consensus_handler.rs
@@ -424,9 +424,6 @@ impl<C: DWalletCheckpointServiceNotify + Send + Sync> ConsensusHandler<C> {
             .epoch_store
             .get_dwallet_mpc_outputs_verifier_write()
             .await;
-        for event in self.epoch_store.tables()?.get_all_dwallet_mpc_events()? {
-            dwallet_mpc_verifier.monitor_new_session_outputs(&event.session_info);
-        }
         for output in self.epoch_store.tables()?.get_all_dwallet_mpc_outputs()? {
             if let Err(err) = dwallet_mpc_verifier
                 .try_verify_output(&output.output, &output.session_info, output.authority)
@@ -436,20 +433,6 @@ impl<C: DWalletCheckpointServiceNotify + Send + Sync> ConsensusHandler<C> {
                     "failed to verify output from session {:?} and party {:?}: {:?}",
                     output.session_info.session_id, output.authority, err
                 );
-            }
-        }
-        for message in self
-            .epoch_store
-            .tables()?
-            .get_all_dwallet_mpc_dwallet_mpc_messages()?
-        {
-            match message {
-                DWalletMPCDBMessage::Message(_)
-                | DWalletMPCDBMessage::EndOfDelivery
-                | DWalletMPCDBMessage::MPCSessionFailed(_)
-                | DWalletMPCDBMessage::MaliciousReport(..)
-                | DWalletMPCDBMessage::PerformCryptographicComputations
-                | DWalletMPCDBMessage::ThresholdNotReachedReport(..) => {}
             }
         }
         Ok(())
