1757a320abe962cf5dc83463954877b22999446c
Itay <itaylevy134@gmail.com>
2025-06-17 18:11:52 +0300

    consensus is not working

--- crates/ika-core/src/consensus_handler.rs
+++ crates/ika-core/src/consensus_handler.rs
@@ -39,7 +39,7 @@ use crate::{
     dwallet_checkpoints::{DWalletCheckpointService, DWalletCheckpointServiceNotify},
     scoring_decision::update_low_scoring_authorities,
 };
-use ika_types::error::{IkaResult};
+use ika_types::error::IkaResult;
 use tokio::task::JoinSet;
 use tracing::{error, info, instrument, trace_span, warn};
 
