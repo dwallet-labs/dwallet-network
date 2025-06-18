c129ff2d77f82b9fca7141298f3ae778435b806f
Itay <itaylevy134@gmail.com>
2025-06-18 13:09:06 +0300

    --wip-- [skip ci]

--- crates/ika-core/src/consensus_handler.rs
+++ crates/ika-core/src/consensus_handler.rs
@@ -41,7 +41,7 @@ use crate::{
 };
 use ika_types::error::IkaResult;
 use tokio::task::JoinSet;
-use tracing::{error, info, instrument, trace_span, warn};
+use tracing::{debug, error, info, instrument, trace_span, warn};
 
 pub struct ConsensusHandlerInitializer {
     state: Arc<AuthorityState>,
@@ -230,7 +230,7 @@ impl<C: DWalletCheckpointServiceNotify + Send + Sync> ConsensusHandler<C> {
             timestamp
         };
 
-        info!(
+        debug!(
             %consensus_commit,
             epoch = ?self.epoch_store.epoch(),
             "Received consensus output"
