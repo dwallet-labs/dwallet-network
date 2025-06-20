ad72812ca5a1806a60ef8efed1e7177eebddeaef
Yehonatan Cohen Scaly <scalys7@gmail.com>
2025-06-20 15:27:47 +0300

    fix build, but still network dkg not working

---
+++ scripts/mock_cg.sh
@@ -0,0 +1,1 @@
+./target/release/ika validator make-validator-info "My Validator" "Secure and fast" "https://example.com/image.png" "https://example.com" "127.0.0.1" 10000 0x95731e3ed8095705658ae12089866431aa1eeba9d7c6fe20683b4e151f809d3c
