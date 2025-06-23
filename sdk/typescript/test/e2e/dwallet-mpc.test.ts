756739655375ef35b96ba75a099e76aebb7e460c
Itay <itaylevy134@gmail.com>
2025-06-23 13:53:41 +0300

    good

--- sdk/typescript/test/e2e/dwallet-mpc.test.ts
+++ sdk/typescript/test/e2e/dwallet-mpc.test.ts
@@ -32,10 +32,10 @@ import {
 } from '../../src/dwallet-mpc/sign';
 
 const fiveMinutes = 10000000 * 60 * 1000;
-// const SUI_FULLNODE_URL = 'https://fullnode.sui.beta.devnet.ika-network.net';
-// const SUI_FAUCET_HOST = 'https://faucet.sui.beta.devnet.ika-network.net';
-const SUI_FULLNODE_URL = getFullnodeUrl('localnet');
-const SUI_FAUCET_HOST = getFaucetHost('localnet');
+const SUI_FULLNODE_URL = 'https://fullnode.sui.beta.devnet.ika-network.net';
+const SUI_FAUCET_HOST = 'https://faucet.sui.beta.devnet.ika-network.net';
+// const SUI_FULLNODE_URL = getFullnodeUrl('localnet');
+// const SUI_FAUCET_HOST = getFaucetHost('localnet');
 
 async function createConf(): Promise<Config> {
 	const keypair = Ed25519Keypair.generate();
