6faa6ec8f24326359955add7c888d78bee6abc28
Itay <itaylevy134@gmail.com>
2025-07-17 15:56:38 +0300

    fix

--- crates/ika-core/src/main.rs
+++ crates/ika-core/src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     let root_seed = base64::decode("Nq6w6BGFA9cSIPYmbQY0Ar+IdKWin8N//0LeFDI0Qik=").unwrap();
     let class_groups_key_pair =
         ClassGroupsKeyPairAndProof::from_seed(&RootSeed::new(root_seed.try_into().unwrap()));
-    println!("{:?}", class_groups_key_pair);
+    println!("generated class groups key pair");
 }
