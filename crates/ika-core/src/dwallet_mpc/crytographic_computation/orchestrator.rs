b7eb9c2879385f5b278e228e8c0c481ab9a66f62
Itay <itaylevy134@gmail.com>
2025-07-20 11:55:11 +0300

    good

--- crates/ika-core/src/dwallet_mpc/crytographic_computation/orchestrator.rs
+++ crates/ika-core/src/dwallet_mpc/crytographic_computation/orchestrator.rs
@@ -82,10 +82,10 @@ impl CryptographicComputationsOrchestrator {
     pub(crate) fn try_new(root_seed: RootSeed) -> DwalletMPCResult<Self> {
         let (report_computation_completed_sender, report_computation_completed_receiver) =
             tokio::sync::mpsc::channel(COMPUTATION_UPDATE_CHANNEL_SIZE);
-        let available_cores_for_computations: usize = std::thread::available_parallelism()
+        let total_cores_available: usize = std::thread::available_parallelism()
             .map_err(|e| DwalletMPCError::FailedToGetAvailableParallelism(e.to_string()))?
-            .into()
-            - TOKIO_ALLOCATED_CORES;
+            .into();
+        let available_cores_for_computations: usize = total_cores_available - TOKIO_ALLOCATED_CORES;
         if available_cores_for_computations == 0 {
             error!(
                 "failed to get available parallelism, no CPU cores available for cryptographic computations"
