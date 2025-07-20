159587f160b0257f4adaf92ea9145a9b072ad7b5
Itay Levy <itaylevy134@gmail.com>
2025-07-20 14:09:33 +0300

    fix: Limit the Rayon thread pool size (#1294)
    (cherry picked from commit 088d32732f62261cb9106623cb49dc61e6db2dbb)

--- crates/ika-core/src/runtime.rs
+++ crates/ika-core/src/runtime.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
 use ika_config::NodeConfig;
+use ika_types::dwallet_mpc_error::DwalletMPCError;
 use tokio::runtime::Runtime;
 use tracing::error;
 
@@ -15,11 +16,16 @@ const SIXTEEN_MEGA_BYTES: usize = 16 * 1024 * 1024;
 
 impl IkaRuntimes {
     pub fn new(_config: &NodeConfig) -> Self {
-        if let Err(err) = rayon::ThreadPoolBuilder::new()
+        let mut builder = rayon::ThreadPoolBuilder::new()
             .panic_handler(|err| error!("Rayon thread pool task panicked: {:?}", err))
-            .stack_size(SIXTEEN_MEGA_BYTES)
-            .build_global()
+            .stack_size(SIXTEEN_MEGA_BYTES);
+        #[cfg(feature = "enforce-minimum-cpu")]
         {
+            // When passing 0, Rayon will use the default number of threads, which is the number of available cores
+            // on the machine
+            builder = builder.num_threads(Self::calculate_num_of_computations_cores());
+        }
+        if let Err(err) = builder.build_global() {
             error!(?err, "failed to create rayon thread pool");
             panic!("Failed to create rayon thread pool");
         }
@@ -38,4 +44,30 @@ impl IkaRuntimes {
 
         Self { ika_node, metrics }
     }
+
+    pub(crate) fn calculate_num_of_computations_cores() -> usize {
+        let Ok(total_cores_available) = std::thread::available_parallelism() else {
+            error!("failed to get available parallelism, using default value");
+            return 0;
+        };
+        let total_cores_available: usize = total_cores_available.into();
+        #[cfg(feature = "enforce-minimum-cpu")]
+        {
+            assert!(
+                total_cores_available >= 16,
+                "Validator must have at least 16 CPU cores"
+            );
+        }
+        if total_cores_available < TOKIO_ALLOCATED_CORES {
+            error!(
+                ?total_cores_available,
+                "available cores are less than TOKIO_ALLOCATED_CORES, using default value"
+            );
+            return 0;
+        }
+        total_cores_available - TOKIO_ALLOCATED_CORES
+    }
 }
+
+/// Number of cores unavailable to cryptographic computation, reserved solely for `tokio` i.e. consensus and network services use.
+pub const TOKIO_ALLOCATED_CORES: usize = 4;
