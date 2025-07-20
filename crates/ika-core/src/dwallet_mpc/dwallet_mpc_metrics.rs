043c1b8ff6882bc5c251c09e4b490687dfda5483
Omer Sadika <omersadika@users.noreply.github.com>
2025-07-20 17:56:17 +0300

    Add last process MPC consensus round metric (#1299)

--- crates/ika-core/src/dwallet_mpc/dwallet_mpc_metrics.rs
+++ crates/ika-core/src/dwallet_mpc/dwallet_mpc_metrics.rs
@@ -91,6 +91,8 @@ pub struct DWalletMPCMetrics {
     pub number_of_expected_sign_sessions: IntGauge,
     /// The number of sign sessions in which less than a quorum of the expected decrypters has participated.
     pub number_of_unexpected_sign_sessions: IntGauge,
+    /// The last process MPC consensus round.
+    pub last_process_mpc_consensus_round: IntGauge,
 }
 
 impl DWalletMPCMetrics {
@@ -185,6 +187,12 @@ impl DWalletMPCMetrics {
             number_of_expected_sign_sessions: register_int_gauge_with_registry!(
                 "dwallet_mpc_number_of_expected_sign_sessions",
                 "Number of expected sign sessions",
+                registry
+            )
+            .unwrap(),
+            last_process_mpc_consensus_round: register_int_gauge_with_registry!(
+                "last_process_mpc_consensus_round",
+                "Last process mpc consensus round",
                 registry
             )
             .unwrap(),
