2c353f426f53f548c26e2d9ea382cb14fe172b4e
Itay <itaylevy134@gmail.com>
2025-07-23 13:48:43 +0300

    comments

--- scripts/update_pricing.sh
+++ scripts/update_pricing.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # This script allows running a pricing update experiment for Ika using Swarm.
+# You set the new Ika fee per validator in the NEW_IKA_FEE_PER_VALIDATOR variable.
+# Then, when running this script, it will send a pricing vote from each operator to its ika fee,
+# i.e. using the below value, from validator 1 it will send a vote to set the ika fee to 100,
+# from validator 2 it will send a vote to set the ika fee to 200, etc.
+
+# After sending the vote, wait for an epoch switch for the votes to take effect.
+
+# Then query the new current price with `./target/debug/ika validator get-current-pricing-info` & verify the
+# new ika fee has been updated correctly.
+
+NEW_IKA_FEE_PER_VALIDATOR="100 200 300 400"
 
 yq -P -o yaml ./ika_config.json > /Users/itaylevy/.ika/ika_config/ika_sui_config.yaml
-./target/debug/ika validator get-current-pricing-info
 ./target/debug/ika validator get-current-pricing-info
 yq -i '(.[] | select(.value.fee_ika) | .value.fee_ika) = 100' current_pricing.yaml
 grep "account_key_pair: A" ~/.ika/ika_config/network.yaml | awk '{print $2}' | while read key; do
@@ -16,9 +26,9 @@ operators_cap_ids="0x06ca7b7c6c2cf947a398e6b3bcf23f6f2e99d788427b0ac76a46ae13571
 0xf2c8d3489389f1a37f85e0872623c15450e31bc68ca99341db95e6c9d2ade5fa \
 0x7ae6306cbe2f590e55717381a48ab789ecf208a61adcca85a9bc9ec92b0522c4"
 
-new_ika_fee_per_validator="100 200 300 400"
 
-paste <(echo "$operators_cap_ids" | tr ' ' '\n') <(echo "$new_ika_fee_per_validator" | tr ' ' '\n') | while read object_id fee; do
+
+paste <(echo "$operators_cap_ids" | tr ' ' '\n') <(echo "$NEW_IKA_FEE_PER_VALIDATOR" | tr ' ' '\n') | while read object_id fee; do
   yq -i "(.[] | select(.value.fee_ika) | .value.fee_ika) = $fee" current_pricing.yaml
   owner_address=$(sui client object "$object_id" | grep 'AddressOwner' | sed -n 's/.*│ *\(0x[a-f0-9]\{64\}\) *│.*/\1/p')
   sui client switch --address "$owner_address"
