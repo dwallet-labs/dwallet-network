922e17c1a7940240cdedd88ddf56419d851822fa
Itay <itaylevy134@gmail.com>
2025-07-23 11:57:00 +0300

    good

--- scripts/update_pricing.sh
+++ scripts/update_pricing.sh
@@ -2,3 +2,6 @@
 yq -P -o yaml ./ika_config.json > /Users/itaylevy/.ika/ika_config/ika_sui_config.yaml
 ./target/debug/ika validator get-current-pricing-info
 yq -i '(.[] | select(.value.fee_ika) | .value.fee_ika) = 100' current_pricing.yaml
+grep "account_key_pair: A" ~/.ika/ika_config/network.yaml | awk '{print $2}' | while read key; do
+  echo "$key"
+done
