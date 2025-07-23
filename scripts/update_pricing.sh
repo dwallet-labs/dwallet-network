f0d8c2ce4850485f3267843ea9a938829d686ceb
Itay <itaylevy134@gmail.com>
2025-07-23 11:51:34 +0300

    sweet

---
+++ scripts/update_pricing.sh
@@ -0,0 +1,3 @@
+./target/debug/ika validator get-current-pricing-info
+yq -P -o yaml ./ika_config.json > /Users/itaylevy/.ika/ika_config/ika_sui_config.yaml
+./target/debug/ika validator get-current-pricing-info
