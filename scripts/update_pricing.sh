ffe62ef1964ba1dbf1595d1d8d37bfec3e1816b5
Itay <itaylevy134@gmail.com>
2025-07-23 12:14:54 +0300

    script is perfect

--- scripts/update_pricing.sh
+++ scripts/update_pricing.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ./target/debug/ika validator get-current-pricing-info
 yq -P -o yaml ./ika_config.json > /Users/itaylevy/.ika/ika_config/ika_sui_config.yaml
 ./target/debug/ika validator get-current-pricing-info
@@ -12,7 +13,10 @@ operators_cap_ids="0x212fe64375a01cee46f31c900e3a8dbac1df3d7928eccc4feed758588bc
 0xbce0bb8411a008bf8f4a84fa61d40ca67a7a31388fabde7ca6e52761c6bb1ceb \
 0xb4a5921cf07174f7ae8adf0cf1308be6aec801122640b59fcb05df455632c5f9"
 
-for object_id in $operators_cap_ids; do
+new_ika_fee_per_validator="100 200 300 400"
+
+paste <(echo "$operators_cap_ids" | tr ' ' '\n') <(echo "$new_ika_fee_per_validator" | tr ' ' '\n') | while read object_id fee; do
+  yq -i "(.[] | select(.value.fee_ika) | .value.fee_ika) = $fee" current_pricing.yaml
   owner_address=$(sui client object "$object_id" | grep 'AddressOwner' | sed -n 's/.*│ *\(0x[a-f0-9]\{64\}\) *│.*/\1/p')
   sui client switch --address "$owner_address"
   ./target/debug/ika validator set-pricing-vote --validator-operation-cap-id "$object_id"  --new-pricing-file-path ./current_pricing.yaml
