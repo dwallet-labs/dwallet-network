afbcb8ec722d2d36f2119a005d71439e962d896a
Itay <itaylevy134@gmail.com>
2025-07-23 11:54:27 +0300

    good

--- scripts/update_pricing.sh
+++ scripts/update_pricing.sh
@@ -1,3 +1,4 @@
 ./target/debug/ika validator get-current-pricing-info
 yq -P -o yaml ./ika_config.json > /Users/itaylevy/.ika/ika_config/ika_sui_config.yaml
 ./target/debug/ika validator get-current-pricing-info
+yq -i '(.[] | select(.value.fee_ika) | .value.fee_ika) = 100' current_pricing.yaml
