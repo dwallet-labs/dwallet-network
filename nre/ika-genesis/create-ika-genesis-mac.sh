6c4a0d60f5ccf545a58bd9e1475f3c8bff020b08
Zeev Manilovich <zeevmanilovich@gmail.com>
2025-07-24 13:07:29 +0300

    wip

--- nre/ika-genesis/create-ika-genesis-mac.sh
+++ nre/ika-genesis/create-ika-genesis-mac.sh
@@ -26,7 +26,7 @@ fi
 # The prefix for the validator names (e.g. val1.devnet.ika.cloud, val2.devnet.ika.cloud, etc...).
 export VALIDATOR_PREFIX="val"
 # The number of validators to create.
-export VALIDATOR_NUM=1
+export VALIDATOR_NUM=4
 # The number of staked tokens for each validator.
 export VALIDATOR_STAKED_TOKENS_NUM=40000000000000000
 # The subdomain for Ika the network.
@@ -519,6 +519,10 @@ done
 #############################
 # IKA System Initialization
 #############################
+
+# sleep 30 seconds (needed for initialize)
+#sleep 30
+echo "sleeping for 30 seconds"
 
 # Copy publisher sui_config to SUI_CONFIG_PATH
 rm -rf "$SUI_CONFIG_PATH"
